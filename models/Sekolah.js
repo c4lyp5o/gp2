f2e6605032490fb24fa468bf46e03ae8c40507bc
naimnaimnaimnaim <naimzafar1995@gmail.com>
2022-03-22 15:30:18 +0800

    fixed model sekolah

--- models/Sekolah.js
+++ models/Sekolah.js
@@ -1,39 +1,39 @@
 const mongoose = require('mongoose');
 
-const sekolahSchema = mongoose.Schema({
- // negeri, daerah, kp is associated with each person
- createdByNegeri: {
-    type: String,
-    required: true
-},
-createdByDaerah: {
-    type: String,
-    required: true
-},
-createdByKp: {
-    type: String,
-    required: true
-},
-// --------------------------------------------------
-namaPendaftaranSekolah: {
-    type: String,
-    required: [true, 'Please provide nama'],
-    trim: true
-},
-umurPendaftaranSekolah: {
-    type: String,
-    required: [true, 'Please provide umur'],
-},
-kelasPendaftaranSekolah: {
-    type: String,
-    required: [true, 'Please provide kelas'],
-    trim: true
-},
-namaSekolahPendaftaranSekolah: {
-    type: String,
-    required: [true, 'Please provide nama sekolah'],
-    trim: true
-}
-});
+const SekolahSchema = mongoose.Schema({
+    // negeri, daerah, kp is associated with each person
+    createdByNegeri: {
+        type: String,
+        required: true
+    },
+    createdByDaerah: {
+        type: String,
+        required: true
+    },
+    createdByKp: {
+        type: String,
+        required: true
+    },
+    // --------------------------------------------------
+    namaPendaftaranSekolah: {
+        type: String,
+        required: [true, 'Please provide nama'],
+        trim: true
+    },
+    umurPendaftaranSekolah: {
+        type: String,
+        required: [true, 'Please provide umur'],
+    },
+    kelasPendaftaranSekolah: {
+        type: String,
+        required: [true, 'Please provide kelas'],
+        trim: true
+    },
+    namaSekolahPendaftaranSekolah: {
+        type: String,
+        required: [true, 'Please provide nama sekolah'],
+        trim: true
+    }
+    });
 
 module.exports = mongoose.model('Sekolah', SekolahSchema);
