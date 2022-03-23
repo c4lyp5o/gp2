f6cd7e9fb7e2b9d66cffdc53aaf08c134cf5cc84
naimnaimnaimnaim <naimzafar1995@gmail.com>
2022-03-23 17:54:26 +0800

    banckend

--- public/modules/1-tadika/tadikaBackend.js
+++ public/modules/1-tadika/tadikaBackend.js
@@ -5,22 +5,42 @@ const namaPendaftaranTadikaDOM = document.getElementById('nama-pendaftaran-tadik
 const umurPendaftaranTadikaDOM = document.getElementById('umur-pendaftaran-tadika');
 const bangsaPendaftaranTadikaDOM = document.getElementById('bangsa-pendaftaran-tadika');
 const kelasPendaftaranTadikaDOM = document.getElementById('kelas-pendaftaran-tadika');
-const namaTaskaTadikaPendaftaranTadikaDOM = document.getElementById('nama-taska/tadika-pendaftaran-tadika');
+const namaTaskaTadikaPendaftaranTadikaDOM = document.getElementById('nama-taskatadika-pendaftaran-tadika');
+
+
+const baruPendaftaranTadikaDOM = document.getElementById('baru-pendaftaran-tadika');
+const ulanganPendaftaranTadikaDOM = document.getElementById('ulangan-pendaftaran-tadika');
+const engganPendaftaranTadikaDOM = document.getElementById('enggan-pendaftaran-tadika');
+const tidakhadirPendaftaranTadikaDOM = document.getElementById('tidak-hadir-pendaftaran-tadika');
+
 
 btnSaveDOM.addEventListener('click', async () => {
     const namaPendaftaranTadika = namaPendaftaranTadikaDOM.value;
     const umurPendaftaranTadika = umurPendaftaranTadikaDOM.value;
     const kelasPendaftaranTadika = kelasPendaftaranTadikaDOM.value;
     const namaTaskaTadikaPendaftaranTadika = namaTaskaTadikaPendaftaranTadikaDOM.value;
+
+    const baruPendaftaranTadika = baruPendaftaranTadikaDOM.value;
+    const ulanganPendaftaranTadika = ulanganPendaftaranTadikaDOM.value;
+    const engganPendaftaranTadika = engganPendaftaranTadikaDOM.value;
+    const tidakhadirPendaftaranTadika = tidakhadirPendaftaranTadikaDOM.value;
     try {
         await axios.post('/api/v1/tadika', { namaPendaftaranTadika,
             umurPendaftaranTadika,
             kelasPendaftaranTadika,
-            namaTaskaTadikaPendaftaranTadika }, { headers: { Authorization: `Bearer ${token}` } });
+            namaTaskaTadikaPendaftaranTadika,
+            baruPendaftaranTadika,
+            ulanganPendaftaranTadika,
+            engganPendaftaranTadika,
+            tidakhadirPendaftaranTadika }, { headers: { Authorization: `Bearer ${token}` } });
         
         // clearing all value after creating person
         namaPendaftaranTadikaDOM.value = '';
         umurPendaftaranTadikaDOM.value = '';
+        baruPendaftaranTadikaDOM.value = '';
+        ulanganPendaftaranTadikaDOM.value = '';
+        engganPendaftaranTadikaDOM.value = '';
+        tidakhadirPendaftaranTadikaDOM.value = '';
         // kelasPendaftaranTadikaDOM.value = '';
         // namaTaskaTadikaPendaftaranTadikaDOM.value = '';
         // ----------------------------------------
