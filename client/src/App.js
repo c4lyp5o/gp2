3b2e619e2d4e8b5d405c20a85b10083785d40f9a
Noct <ezy_95@outlook.com>
2022-04-11 10:31:06 +0800

    add root css

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './admin/admin.css';
+import AdminHeader from './admin/adminComponent/AdminHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <AdminHeader />
       </header>
     </div>
   );
---
+++ client/src/admin/adminComponent/AdminHeader.js
@@ -0,0 +1,16 @@
+function AdminHeader() {
+  return (
+    <div className='admin-header-container'>
+      <div className='jata-container'>
+        <img src='' alt='missing jata negara' />
+        <p>kementerian kesihatan malaysia</p>
+        <p>program kesihatan pergigian</p>
+      </div>
+      <div className='text-container'>
+        <h1>admin sistem gi-Ret PSY 2.0</h1>
+      </div>
+    </div>
+  );
+}
+
+export default AdminHeader;
