15c0709c1876d2489c89603897a0d839f1ad4bee
c4lyp5o <calypso.hellhammer@gmail.com>
2022-04-11 12:21:23 +0800

    done footer and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 // import './App.css';
 import './admin/admin.css';
-import AdminHeader from './admin/adminComponent/AdminHeader';
+import AdminFooter from './admin/components/AdminFooter';
+import AdminHeader from './admin/components/AdminHeader';
+import AdminLoginForm from './admin/components/AdminLoginForm';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <AdminHeader />
+        <AdminLoginForm />
+        <AdminFooter />
       </header>
     </div>
   );
--- client/src/admin/components/AdminLoginForm.js
+++ client/src/admin/components/AdminLoginForm.js
@@ -1,15 +1,28 @@
-import './admin/admin.css';
+import '../admin.css';
 
-function Starter() {
+function AdminLoginForm() {
     return (
         <>
-        <div className='strip-atas'></div>
-        <div className="starter-template">
-            <h1>Starter</h1>
-            <p className="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
+        <div className="admin-form-container">
+            <div className="admin-form-box">
+                <div className="admin-form-box-header">
+                    <span className="admin-form-box-header-text">SILA MASUKKAN KATA LALUAN</span>
+                </div>
+                <div className="admin-form-box-body">
+                    <div className="admin-form-box-body-input-container">
+                        <input className="admin-form-box-body-input" type="text" placeholder="ID Pengguna" /><br />
+                        <input className="admin-form-box-body-input" type="text" placeholder="Kata Laluan" />
+                        <a className="linkLupa" href="/">Lupa kata laluan</a>
+                    </div>
+                    <br />
+                    <div className="admin-form-box-body-button-container">
+                        <button className="admin-form-box-body-button">MASUK</button>
+                    </div>
+                </div>
+            </div>
         </div>
         </>        
     );
 }
 
-export default Starter;
+export default AdminLoginForm;
