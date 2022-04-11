07405f106cd866edbebc4807e1c6d78509b3ad40
c4lyp5o <calypso.hellhammer@gmail.com>
2022-04-11 16:22:33 +0800

    siapkan admin header

--- client/src/admin/components/AdminHeader.js
+++ client/src/admin/components/AdminHeader.js
@@ -1,13 +1,23 @@
+import AdminHeaderLoggedIn from "./AdminHeaderLoggedIn";
+
 function AdminHeader() {
   return (
     <div className='admin-header-container'>
       <div className='jata-container'>
-        <img src='' alt='missing jata negara' />
+        <img
+          width={100}
+          height={100}
+          src='https://upload.wikimedia.org/wikipedia/commons/9/94/Jata_MalaysiaV2.svg'
+          alt='missing jata negara'
+        />
         <p>kementerian kesihatan malaysia</p>
         <p>program kesihatan pergigian</p>
       </div>
       <div className='text-container'>
         <h1>admin sistem gi-Ret PSY 2.0</h1>
+      </div>
+      <div className='admin-header-logged-in-container'>
+        <AdminHeaderLoggedIn />
       </div>
     </div>
   );
