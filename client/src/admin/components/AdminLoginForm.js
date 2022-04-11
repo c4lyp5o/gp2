bf025011ad26fd46e5d9b71757e4bcd1a0390f89
c4lyp5o <calypso.hellhammer@gmail.com>
2022-04-11 10:33:09 +0800

    terpaksa

---
+++ client/src/admin/components/AdminLoginForm.js
@@ -0,0 +1,15 @@
+import './admin/admin.css';
+
+function Starter() {
+    return (
+        <>
+        <div className='strip-atas'></div>
+        <div className="starter-template">
+            <h1>Starter</h1>
+            <p className="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
+        </div>
+        </>        
+    );
+}
+
+export default Starter;
