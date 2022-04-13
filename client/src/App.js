9c42a11a084a9eabead5ecebc77717e4c1b0b2d7
c4lyp5o <calypso.hellhammer@gmail.com>
2022-04-13 12:15:36 +0800

    slight changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 // import './App.css';
-import './admin/admin.css';
-import AdminHeader from './admin/components/AdminHeader';
+import "./admin/admin.css";
+import AdminHeader from "./admin/components/AdminHeader";
 // import AdminHeaderLoggedIn from './admin/components/AdminHeaderLoggedIn';
-import AdminNavbar from './admin/components/AdminNavbar';
-import AdminLoginForm from './admin/components/AdminLoginForm';
-import AdminSelamatDatang from './admin/components/AdminSelamatDatang';
-import AdminFooter from './admin/components/AdminFooter';
+import AdminNavbar from "./admin/components/AdminNavbar";
+import AdminLoginForm from "./admin/components/AdminLoginForm";
+import AdminSelamatDatang from "./admin/components/AdminSelamatDatang";
+import AdminFooter from "./admin/components/AdminFooter";
 
 function App() {
   return (
-    <div className='canvas'>
+    <div className="canvas">
       <AdminHeader />
       {/* <AdminHeaderLoggedIn /> */}
       <AdminNavbar />
--- client/src/admin/components/AdminSelamatDatang.js
+++ client/src/admin/components/AdminSelamatDatang.js
@@ -3,15 +3,15 @@ import AdminTable from "./AdminTable";
 import AdminPegawaiHandler from "./AdminPegawaiHandler";
 
 function AdminSelamatDatang() {
-    return (
-        <div className="admin-selamat-datang">
-            <div className="admin-selamat-datang-container">
-                {/* <AdminSelamatDatangWelcomeText /> */}
-                {/* <AdminTable /> */}
-                {/* <AdminPegawaiHandler /> */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="admin-selamat-datang">
+      <div className="admin-selamat-datang-container">
+        {/* <AdminSelamatDatangWelcomeText /> */}
+        {/* <AdminTable /> */}
+        <AdminPegawaiHandler />
+      </div>
+    </div>
+  );
 }
 
 export default AdminSelamatDatang;
