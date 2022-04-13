8251bc0e436283090533cdaebb5ac8a6bf329ce5
Noct <ezy_95@outlook.com>
2022-04-13 09:14:08 +0800

    siap navbar

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className='canvas'>
       <AdminHeader />
-      {/* <AdminHeaderLoggedIn /> */}
       <AdminNavbar />
       <AdminLoginForm />
       <AdminFooter />
--- client/src/admin/components/AdminNavbar.js
+++ client/src/admin/components/AdminNavbar.js
@@ -1,15 +1,33 @@
+import { useState, useRef, useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
 
 function AdminNavbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight + 20}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return (
     <nav className='nav-container'>
       <div className='nav-header'>
-        <button className='nav-toggle'>
+        <button className='nav-toggle' onClick={toggleLinks}>
           <FaBars />
         </button>
       </div>
-      <div className='links-container'>
-        <ul className='links'>
+      <div className='links-container' ref={linksContainerRef}>
+        <ul className='links' ref={linksRef}>
           <li>
             <a href='#dashboard'>dashboard</a>
           </li>
