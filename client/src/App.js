4877b2fd900cd7ca216e2911ba568defee037505
Noct <ezy_95@outlook.com>
2022-04-13 11:03:41 +0800

    nak template selamat dtg

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
-// import './App.css';
-import './admin/admin.css';
-import AdminHeader from './admin/components/AdminHeader';
-import AdminNavbar from './admin/components/AdminNavbar';
-import AdminLoginForm from './admin/components/AdminLoginForm';
-import AdminFooter from './admin/components/AdminFooter';
+// import './admin/admin.css';
+// import AdminHeader from './admin/components/AdminHeader';
+// import AdminNavbar from './admin/components/AdminNavbar';
+// import AdminLoginForm from './admin/components/AdminLoginForm';
+// import AdminFooter from './admin/components/AdminFooter';
+
+import './user/user.css';
+import UserHeader from './user/components/UserHeader';
+import UserNavbar from './user/components/UserNavbar';
+import UserLoginForm from './user/components/UserLoginForm';
+import UserPilihNama from './user/components/UserPilihNama';
+import UserFooter from './user/components/UserFooter';
 
 function App() {
   return (
-    <div className='canvas'>
-      <AdminHeader />
-      <AdminNavbar />
-      <AdminLoginForm />
-      <AdminFooter />
+    // <div className='admin-canvas'>
+    //   <AdminHeader />
+    //   <AdminNavbar />
+    //   <AdminLoginForm />
+    //   <AdminFooter />
+    // </div>
+    <div className='user-canvas'>
+      <UserHeader />
+      <UserNavbar />
+      {/* <UserLoginForm /> */}
+      <UserPilihNama />
+      <UserFooter />
     </div>
   );
 }
---
+++ client/src/user/components/UserPilihNama.js
@@ -0,0 +1,11 @@
+function UserPilihNama() {
+  return (
+    <div className='user-pilih-nama-container'>
+      <div className='user-pilih-nama-box'>
+        <h3>selamat datang klinik pergigian alor janggus</h3>
+      </div>
+    </div>
+  );
+}
+
+export default UserPilihNama;
