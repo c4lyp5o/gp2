9921cc85d7026fd240d2c8ec3cbb4c9b2bd95c66
Noct <ezy_95@outlook.com>
2022-04-15 03:06:45 +0800

    to pc

--- client/src/user/components/UserDashboard.js
+++ client/src/user/components/UserDashboard.js
@@ -3,7 +3,14 @@ function UserDashboard() {
     <>
       <button>jumlah data entri</button>
       <section className='jumlah-data-entri-container'>
-        <div>jumlah kedatangan</div>
+        <div className='jumlah-kedatangan-container'>
+          <div className='jumlah-kedatangan'>jumlah kedatangan</div>
+          <div className='jumlah-baru-ulangan'>6734</div>
+          <div className='baru'>baru</div>
+          <div className='jumlah-baru'>5389</div>
+          <div className='ulangan'>ulangan</div>
+          <div className='jumlah-ulangan'>1345</div>
+        </div>
         <div>klinik pergigian</div>
         <div>taska</div>
         <div>tadika</div>
