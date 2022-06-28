281f1927d4519df2cc27855dd6317085b9ce5a47
Leong <leongjiazheng96@gmail.com>
2022-06-28 10:02:16 +0800

    add PG211 partial & pg214

---
+++ client/src/admin/unused/PG214-LeongMapping .js
@@ -0,0 +1,163 @@
+//PG214
+
+//Kategori Umur 60 Tahun
+
+let rowNew = worksheet.getRow(13);
+rowNew.getCell(3).value = results.melayu; //C13	Kategori Umur 60 Tahun
+rowNew.getCell(4).value = results.cina; //D13	Kategori Umur 60 Tahun
+rowNew.getCell(5).value = results.india; //E13	Kategori Umur 60 Tahun
+rowNew.getCell(6).value = results.bajau; //F13	Kategori Umur 60 Tahun
+rowNew.getCell(7).value = results.dusun; //G13	Kategori Umur 60 Tahun
+rowNew.getCell(8).value = results.kadazan; //H13 Kategori Umur 60 Tahun
+rowNew.getCell(9).value = results.murut; //I13	Kategori Umur 60 Tahun
+rowNew.getCell(10).value = results.bumiputeraSabahLain; //J13 Kategori Umur 60 Tahun
+rowNew.getCell(11).value = results.melanau; //K13 Kategori Umur 60 Tahun
+rowNew.getCell(12).value = results.kedayan; //L13 Kategori Umur 60 Tahun
+rowNew.getCell(13).value = results.iban; //M13 Kategori Umur 60 Tahun
+rowNew.getCell(14).value = results.bidayuh; //N13 Kategori Umur 60 Tahun
+rowNew.getCell(15).value = results.bumiputeraSarawakLain; //O13 Kategori Umur 60 Tahun
+rowNew.getCell(16).value = results.orangAsliSemenanjung; //P13 Kategori Umur 60 Tahun
+rowNew.getCell(17).value = results.lainLainKaum; //Q13 Kategori Umur 60 Tahun
+rowNew.getCell(18).value = results.bukanWarganegara; //R13 Kategori Umur 60 Tahun
+rowNew.getCell(19).value = results.lelaki; //S13 Kategori Umur 60 Tahun
+rowNew.getCell(20).value = results.perempuan; //T13 Kategori Umur 60 Tahun
+rowNew.getCell(21).value = results.edentulous; //U13 Kategori Umur 60 Tahun
+rowNew.getCell(22).value = results.gigiSamaAtauLebihDari20Batang; //V13 Kategori Umur 60 Tahun
+rowNew.getCell(23).value = results.gigiKurangDari20Batang; //W13 Kategori Umur 60 Tahun
+rowNew.getCell(24).value = results.bilGigi; //W13 Kategori Umur 60 Tahun
+rowNew.commit();
+
+//Kategori Umur 61 hingga 64 Tahun
+
+let rowNew2 = worksheet.getRow(14);
+rowNew2.getCell(3).value = results.melayu; //C14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(4).value = results.cina; //D14	Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(5).value = results.india; //E14	Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(6).value = results.bajau; //F14	Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(7).value = results.dusun; //G14	Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(8).value = results.kadazan; //H14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(9).value = results.murut; //I14	Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(10).value = results.bumiputeraSabahLain; //J14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(11).value = results.melanau; //K14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(12).value = results.kedayan; //L14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(13).value = results.iban; //M14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(14).value = results.bidayuh; //N14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(15).value = results.bumiputeraSarawakLain; //O14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(16).value = results.orangAsliSemenanjung; //P14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(17).value = results.lainLainKaum; //Q14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(18).value = results.bukanWarganegara; //R14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(19).value = results.lelaki; //S14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(20).value = results.perempuan; //T14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(21).value = results.edentulous; //U14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(22).value = results.gigiSamaAtauLebihDari20Batang; //V14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(23).value = results.gigiKurangDari20Batang; //W14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.getCell(24).value = results.bilGigi; //W14 Kategori Umur 61 hingga 64 Tahun
+rowNew2.commit();
+
+//Kategori Umur 65 tahun
+
+let rowNew3 = worksheet.getRow(15);
+rowNew3.getCell(3).value = results.melayu; //C15 Kategori Umur 65 Tahun
+rowNew3.getCell(4).value = results.cina; //D15	Kategori Umur 65 Tahun
+rowNew3.getCell(5).value = results.india; //E15	Kategori Umur 65 Tahun
+rowNew3.getCell(6).value = results.bajau; //F15	Kategori Umur 65 Tahun
+rowNew3.getCell(7).value = results.dusun; //G15	Kategori Umur 65 Tahun
+rowNew3.getCell(8).value = results.kadazan; //H15 Kategori Umur 65 Tahun
+rowNew3.getCell(9).value = results.murut; //I15	Kategori Umur 65 Tahun
+rowNew3.getCell(10).value = results.bumiputeraSabahLain; //J15 Kategori Umur 65 Tahun
+rowNew3.getCell(11).value = results.melanau; //K15 Kategori Umur 65 Tahun
+rowNew3.getCell(12).value = results.kedayan; //L15 Kategori Umur 65 Tahun
+rowNew3.getCell(13).value = results.iban; //M15 Kategori Umur 65 Tahun
+rowNew3.getCell(14).value = results.bidayuh; //N15 Kategori Umur 65 Tahun
+rowNew3.getCell(15).value = results.bumiputeraSarawakLain; //O15 Kategori Umur 65 Tahun
+rowNew3.getCell(16).value = results.orangAsliSemenanjung; //P15 Kategori Umur 65 Tahun
+rowNew3.getCell(17).value = results.lainLainKaum; //Q15 Kategori Umur 65 Tahun
+rowNew3.getCell(18).value = results.bukanWarganegara; //R15 Kategori Umur 65 Tahun
+rowNew3.getCell(19).value = results.lelaki; //S15 Kategori Umur 65 Tahun
+rowNew3.getCell(20).value = results.perempuan; //T15 Kategori Umur 65 Tahun
+rowNew3.getCell(21).value = results.edentulous; //U15 Kategori Umur 65 Tahun
+rowNew3.getCell(22).value = results.gigiSamaAtauLebihDari20Batang; //V15 Kategori Umur 65 Tahun
+rowNew3.getCell(23).value = results.gigiKurangDari20Batang; //W15 Kategori Umur 65 Tahun
+rowNew3.getCell(24).value = results.bilGigi; //W15 Kategori Umur 65 Tahun
+rowNew3.commit();
+
+
+//Kategori Umur 66 hingga 69 Tahun
+let rowNew4 = worksheet.getRow(16);
+rowNew4.getCell(3).value = results.melayu; //C16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(4).value = results.cina; //D16	Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(5).value = results.india; //E16	Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(6).value = results.bajau; //F16	Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(7).value = results.dusun; //G16	Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(8).value = results.kadazan; //H16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(9).value = results.murut; //I16	Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(10).value = results.bumiputeraSabahLain; //J16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(11).value = results.melanau; //K16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(12).value = results.kedayan; //L16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(13).value = results.iban; //M16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(14).value = results.bidayuh; //N16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(15).value = results.bumiputeraSarawakLain; //O16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(16).value = results.orangAsliSemenanjung; //P16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(17).value = results.lainLainKaum; //Q16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(18).value = results.bukanWarganegara; //R16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(19).value = results.lelaki; //S16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(20).value = results.perempuan; //T16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(21).value = results.edentulous; //U16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(22).value = results.gigiSamaAtauLebihDari20Batang; //V16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(23).value = results.gigiKurangDari20Batang; //W16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.getCell(24).value = results.bilGigi; //W16 Kategori Umur 66 hingga 69 Tahun
+rowNew4.commit();
+
+//Kategori Umur 70 hingga 74 Tahun
+
+let rowNew5 = worksheet.getRow(17);
+rowNew5.getCell(3).value = results.melayu; //C17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(4).value = results.cina; //D17	Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(5).value = results.india; //E17	Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(6).value = results.bajau; //F17	Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(7).value = results.dusun; //G17	Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(8).value = results.kadazan; //H17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(9).value = results.murut; //I17	Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(10).value = results.bumiputeraSabahLain; //J17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(11).value = results.melanau; //K17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(12).value = results.kedayan; //L17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(13).value = results.iban; //M17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(14).value = results.bidayuh; //N17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(15).value = results.bumiputeraSarawakLain; //O17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(16).value = results.orangAsliSemenanjung; //P17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(17).value = results.lainLainKaum; //Q17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(18).value = results.bukanWarganegara; //R17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(19).value = results.lelaki; //S17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(20).value = results.perempuan; //T17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(21).value = results.edentulous; //U17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(22).value = results.gigiSamaAtauLebihDari20Batang; //V17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(23).value = results.gigiKurangDari20Batang; //W17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.getCell(24).value = results.bilGigi; //W17 Kategori Umur 70 hingga 74 Tahun
+rowNew5.commit();
+
+//Kategori Umur 75 tahun atau lebih
+
+let rowNew6 = worksheet.getRow(18);
+rowNew6.getCell(3).value = results.melayu; //C18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(4).value = results.cina; //D18	Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(5).value = results.india; //E18	Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(6).value = results.bajau; //F18	Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(7).value = results.dusun; //G18	Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(8).value = results.kadazan; //H18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(9).value = results.murut; //I18	Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(10).value = results.bumiputeraSabahLain; //J18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(11).value = results.melanau; //K18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(12).value = results.kedayan; //L18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(13).value = results.iban; //M18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(14).value = results.bidayuh; //N18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(15).value = results.bumiputeraSarawakLain; //O18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(16).value = results.orangAsliSemenanjung; //P18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(17).value = results.lainLainKaum; //Q18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(18).value = results.bukanWarganegara; //R18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(19).value = results.lelaki; //S18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(20).value = results.perempuan; //T18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(21).value = results.edentulous; //U18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(22).value = results.gigiSamaAtauLebihDari20Batang; //V18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(23).value = results.gigiKurangDari20Batang; //W18 Kategori Umur 75 tahun atau lebih
+rowNew6.getCell(24).value = results.bilGigi; //W18 Kategori Umur 75 tahun atau lebih
+rowNew6.commit();
