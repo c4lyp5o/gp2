eb3e77af81ecff613d8ce6f6a99ff49f9476bd7d
hadi <saifulhadi013@gmail.com>
2022-06-29 14:45:39 +0800

    CPPC1

---
+++ client/src/admin/unused/CPPC 1.js
@@ -0,0 +1,238 @@
+//CPPC1
+//No. of new attendance
+let rowNew1 = worksheet.getRow(8);
+rowNew1.getCell(4).value = results.preschool5Y; //          No. of new attendance
+rowNew1.getCell(5).value = results.preschool6Y; //          No. of new attendance
+rowNew1.getCell(6).value = results.class1; //          No. of new attendance
+rowNew1.getCell(7).value = results.class2; //          No. of new attendance
+rowNew1.getCell(8).value = results.class3; //          No. of new attendance
+rowNew1.getCell(9).value = results.class4; //          No. of new attendance
+rowNew1.getCell(10).value = results.class5; //          No. of new attendance
+rowNew1.getCell(11).value = results.class6; //          No. of new attendance
+rowNew1.getCell(12).value = results.peralihan; //          No. of new attendance
+rowNew1.getCell(13).value = results.kKI; //          No. of new attendance
+rowNew1.getCell(14).value = results.total; //          No. of new attendance
+rowNew1.commit();
+
+//CPPC1
+//No of subject require FS 'b'
+let rowNew2 = worksheet.getRow(9);
+rowNew2.getCell(4).value = results.preschool5Y; //          No of subject require FS 'b'
+rowNew2.getCell(5).value = results.preschool6Y; //          No of subject require FS 'b'
+rowNew2.getCell(6).value = results.class1; //          No of subject require FS 'b'
+rowNew2.getCell(7).value = results.class2; //          No of subject require FS 'b'
+rowNew2.getCell(8).value = results.class3; //          No of subject require FS 'b'
+rowNew2.getCell(9).value = results.class4; //          No of subject require FS 'b'
+rowNew2.getCell(10).value = results.class5; //          No of subject require FS 'b'
+rowNew2.getCell(11).value = results.class6; //          No of subject require FS 'b'
+rowNew2.getCell(12).value = results.peralihan; //          No of subject require FS 'b'
+rowNew2.getCell(13).value = results.kKI; //          No of subject require FS 'b'
+rowNew2.getCell(14).value = results.total; //          No of subject require FS 'b'
+rowNew2.commit();
+//CPPC1
+//No of subject require FS 'c'
+let rowNew3 = worksheet.getRow(10);
+rowNew3.getCell(4).value = results.preschool5Y; //          No of subject require FS 'c'
+rowNew3.getCell(5).value = results.preschool6Y; //          No of subject require FS 'c'
+rowNew3.getCell(6).value = results.class1; //          No of subject require FS 'c'
+rowNew3.getCell(7).value = results.class2; //          No of subject require FS 'c'
+rowNew3.getCell(8).value = results.class3; //          No of subject require FS 'c'
+rowNew3.getCell(9).value = results.class4; //          No of subject require FS 'c'
+rowNew3.getCell(10).value = results.class5; //          No of subject require FS 'c'
+rowNew3.getCell(11).value = results.class6; //          No of subject require FS 'c'
+rowNew3.getCell(12).value = results.peralihan; //          No of subject require FS 'c'
+rowNew3.getCell(13).value = results.kKI; //          No of subject require FS 'c'
+rowNew3.getCell(14).value = results.total; //          No of subject require FS 'c'
+rowNew3.commit();
+
+//CPPC1
+//No of subject rendered  FS 'd'
+let rowNew4 = worksheet.getRow(12);
+rowNew4.getCell(4).value = results.preschool5Y; //          No of subject rendered  FS 'd'
+rowNew4.getCell(5).value = results.preschool6Y; //          No of subject rendered  FS 'd'
+rowNew4.getCell(6).value = results.class1; //          No of subject rendered  FS 'd'
+rowNew4.getCell(7).value = results.class2; //          No of subject rendered  FS 'd'
+rowNew4.getCell(8).value = results.class3; //          No of subject rendered  FS 'd'
+rowNew4.getCell(9).value = results.class4; //          No of subject rendered  FS 'd'
+rowNew4.getCell(10).value = results.class5; //          No of subject rendered  FS 'd'
+rowNew4.getCell(11).value = results.class6; //          No of subject rendered  FS 'd'
+rowNew4.getCell(12).value = results.peralihan; //          No of subject rendered  FS 'd'
+rowNew4.getCell(13).value = results.kKI; //          No of subject rendered  FS 'd'
+rowNew4.getCell(14).value = results.total; //          No of subject rendered  FS 'd'
+rowNew4.commit();
+
+//CPPC1
+//No of subject rendered  FS 'e'
+let rowNew5 = worksheet.getRow(14);
+rowNew5.getCell(4).value = results.preschool5Y; //          No of subject rendered  FS 'e'
+rowNew5.getCell(5).value = results.preschool6Y; //          No of subject rendered  FS 'e'
+rowNew5.getCell(6).value = results.class1; //          No of subject rendered  FS 'e'
+rowNew5.getCell(7).value = results.class2; //          No of subject rendered  FS 'e'
+rowNew5.getCell(8).value = results.class3; //          No of subject rendered  FS 'e'
+rowNew5.getCell(9).value = results.class4; //          No of subject rendered  FS 'e'
+rowNew5.getCell(10).value = results.class5; //          No of subject rendered  FS 'e'
+rowNew5.getCell(11).value = results.class6; //          No of subject rendered  FS 'e'
+rowNew5.getCell(12).value = results.peralihan; //          No of subject rendered  FS 'e'
+rowNew5.getCell(13).value = results.kKI; //          No of subject rendered  FS 'e'
+rowNew5.getCell(14).value = results.total; //          No of subject rendered  FS 'e'
+rowNew5.commit();
+
+//CPPC1
+//PrrType 1 Treatment Needed f
+let rowNew6 = worksheet.getRow(16);
+rowNew6.getCell(4).value = results.preschool5Y; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(5).value = results.preschool6Y; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(6).value = results.class1; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(7).value = results.class2; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(8).value = results.class3; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(9).value = results.class4; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(10).value = results.class5; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(11).value = results.class6; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(12).value = results.peralihan; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(13).value = results.kKI; //          PrrType 1 Treatment Needed f
+rowNew6.getCell(14).value = results.total; //          PrrType 1 Treatment Needed f
+rowNew6.commit();
+
+//CPPC1
+//PrrType 1 Treatment Needed g
+let rowNew7 = worksheet.getRow(17);
+rowNew7.getCell(4).value = results.preschool5Y; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(5).value = results.preschool6Y; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(6).value = results.class1; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(7).value = results.class2; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(8).value = results.class3; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(9).value = results.class4; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(10).value = results.class5; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(11).value = results.class6; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(12).value = results.peralihan; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(13).value = results.kKI; //          PrrType 1 Treatment Needed g
+rowNew7.getCell(14).value = results.total; //          PrrType 1 Treatment Needed g
+rowNew7.commit();
+
+//CPPC1
+//PrrType 1  Rendered i
+let rowNew8 = worksheet.getRow(21);
+rowNew8.getCell(4).value = results.preschool5Y; //          PrrType 1  Rendered i
+rowNew8.getCell(5).value = results.preschool6Y; //          PrrType 1  Rendered i
+rowNew8.getCell(6).value = results.class1; //          PrrType 1  Rendered i
+rowNew8.getCell(7).value = results.class2; //          PrrType 1  Rendered i
+rowNew8.getCell(8).value = results.class3; //          PrrType 1  Rendered i
+rowNew8.getCell(9).value = results.class4; //          PrrType 1  Rendered i
+rowNew8.getCell(10).value = results.class5; //          PrrType 1  Rendered i
+rowNew8.getCell(11).value = results.class6; //          PrrType 1  Rendered i
+rowNew8.getCell(12).value = results.peralihan; //          PrrType 1  Rendered i
+rowNew8.getCell(13).value = results.kKI; //          PrrType 1  Rendered i
+rowNew8.getCell(14).value = results.total; //          PrrType 1  Rendered i
+rowNew8.commit();
+
+//CPPC1
+//PrrType 1  Rendered j
+let rowNew9 = worksheet.getRow(23);
+rowNew9.getCell(4).value = results.preschool5Y; //          PrrType 1  Rendered j
+rowNew9.getCell(5).value = results.preschool6Y; //          PrrType 1  Rendered j
+rowNew9.getCell(6).value = results.class1; //          PrrType 1  Rendered j
+rowNew9.getCell(7).value = results.class2; //          PrrType 1  Rendered j
+rowNew9.getCell(8).value = results.class3; //          PrrType 1  Rendered j
+rowNew9.getCell(9).value = results.class4; //          PrrType 1  Rendered j
+rowNew9.getCell(10).value = results.class5; //          PrrType 1  Rendered j
+rowNew9.getCell(11).value = results.class6; //          PrrType 1  Rendered j
+rowNew9.getCell(12).value = results.peralihan; //          PrrType 1  Rendered j
+rowNew9.getCell(13).value = results.kKI; //          PrrType 1  Rendered j
+rowNew9.getCell(14).value = results.total; //          PrrType 1  Rendered j
+rowNew9.commit();
+
+//CPPC1
+//Fluoride Varnish Treatment Needed j
+let rowNew10 = worksheet.getRow(23);
+rowNew10.getCell(4).value = results.preschool5Y; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(5).value = results.preschool6Y; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(6).value = results.class1; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(7).value = results.class2; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(8).value = results.class3; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(9).value = results.class4; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(10).value = results.class5; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(11).value = results.class6; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(12).value = results.peralihan; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(13).value = results.kKI; //          Fluoride Varnish Treatment Needed j
+rowNew10.getCell(14).value = results.total; //          Fluoride Varnish Treatment Needed j
+rowNew10.commit();
+
+//CPPC1
+//Fluoride Varnish Treatment Needed k
+let rowNew11 = worksheet.getRow(24);
+rowNew11.getCell(4).value = results.preschool5Y; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(5).value = results.preschool6Y; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(6).value = results.class1; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(7).value = results.class2; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(8).value = results.class3; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(9).value = results.class4; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(10).value = results.class5; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(11).value = results.class6; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(12).value = results.peralihan; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(13).value = results.kKI; //          Fluoride Varnish Treatment Needed k
+rowNew11.getCell(14).value = results.total; //          Fluoride Varnish Treatment Needed k
+rowNew11.commit();
+
+//CPPC1
+//Fluoride Varnish Rendered l
+let rowNew12 = worksheet.getRow(26);
+rowNew12.getCell(4).value = results.preschool5Y; //          Fluoride Varnish Rendered l
+rowNew12.getCell(5).value = results.preschool6Y; //          Fluoride Varnish Rendered l
+rowNew12.getCell(6).value = results.class1; //          Fluoride Varnish Rendered l
+rowNew12.getCell(7).value = results.class2; //          Fluoride Varnish Rendered l
+rowNew12.getCell(8).value = results.class3; //          Fluoride Varnish Rendered l
+rowNew12.getCell(9).value = results.class4; //          Fluoride Varnish Rendered l
+rowNew12.getCell(10).value = results.class5; //          Fluoride Varnish Rendered l
+rowNew12.getCell(11).value = results.class6; //          Fluoride Varnish Rendered l
+rowNew12.getCell(12).value = results.peralihan; //          Fluoride Varnish Rendered l
+rowNew12.getCell(13).value = results.kKI; //          Fluoride Varnish Rendered l
+rowNew12.getCell(14).value = results.total; //          Fluoride Varnish Rendered l
+rowNew12.commit();
+
+//CPPC1
+//Fluoride Varnish Rendered m
+let rowNew13 = worksheet.getRow(28);
+rowNew13.getCell(4).value = results.preschool5Y; //          Fluoride Varnish Rendered m
+rowNew13.getCell(5).value = results.preschool6Y; //          Fluoride Varnish Rendered m
+rowNew13.getCell(6).value = results.class1; //          Fluoride Varnish Rendered m
+rowNew13.getCell(7).value = results.class2; //          Fluoride Varnish Rendered m
+rowNew13.getCell(8).value = results.class3; //          Fluoride Varnish Rendered m
+rowNew13.getCell(9).value = results.class4; //          Fluoride Varnish Rendered m
+rowNew13.getCell(10).value = results.class5; //          Fluoride Varnish Rendered m
+rowNew13.getCell(11).value = results.class6; //          Fluoride Varnish Rendered m
+rowNew13.getCell(12).value = results.peralihan; //          Fluoride Varnish Rendered m
+rowNew13.getCell(13).value = results.kKI; //          Fluoride Varnish Rendered m
+rowNew13.getCell(14).value = results.total; //          Fluoride Varnish Rendered m
+rowNew13.commit();
+
+//CPPC1
+//Caries Free Status n
+let rowNew14 = worksheet.getRow(30);
+rowNew14.getCell(4).value = results.preschool5Y; //          Caries Free Status n
+rowNew14.getCell(5).value = results.preschool6Y; //          Caries Free Status n
+rowNew14.getCell(6).value = results.class1; //          Caries Free Status n
+rowNew14.getCell(7).value = results.class2; //          Caries Free Status n
+rowNew14.getCell(8).value = results.class3; //          Caries Free Status n
+rowNew14.getCell(9).value = results.class4; //          Caries Free Status n
+rowNew14.getCell(10).value = results.class5; //          Caries Free Status n
+rowNew14.getCell(11).value = results.class6; //          Caries Free Status n
+rowNew14.getCell(12).value = results.peralihan; //          Caries Free Status n
+rowNew14.getCell(13).value = results.kKI; //          Caries Free Status n
+rowNew14.getCell(14).value = results.total; //          Caries Free Status n
+rowNew14.commit();
+
+//CPPC1
+//DMFT status o
+let rowNew15 = worksheet.getRow(32);
+rowNew15.getCell(4).value = results.preschool5Y; //          DMFT status o
+rowNew15.getCell(5).value = results.preschool6Y; //          DMFT status o
+rowNew15.getCell(6).value = results.class1; //          DMFT status o
+rowNew15.getCell(7).value = results.class2; //          DMFT status o
+rowNew15.getCell(8).value = results.class3; //          DMFT status o
+rowNew15.getCell(9).value = results.class4; //          DMFT status o
+rowNew15.getCell(10).value = results.class5; //          DMFT status o
+rowNew15.getCell(11).value = results.class6; //          DMFT status o
+rowNew15.getCell(12).value = results.peralihan; //          DMFT status o
+rowNew15.getCell(13).value = results.kKI; //          DMFT status o
+rowNew15.getCell(14).value = results.total; //          DMFT status o
+rowNew15.commit();
