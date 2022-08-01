85e914289c17267a09c8418c9cbfaad7a3b1165f
noct <ezy_95@outlook.com>
2022-08-01 10:33:21 +0800

    fix model Fasiliti

--- models/Fasiliti.js
+++ models/Fasiliti.js
@@ -26,10 +26,6 @@ const fasilitiSchema = new Schema({
   jenisFasiliti: {
     type: String,
   },
-  kodSekolah: {
-    type: String,
-    default: 'NOT APPLICABLE',
-  },
   melaksanakanBegin: {
     type: Boolean,
     default: false,
