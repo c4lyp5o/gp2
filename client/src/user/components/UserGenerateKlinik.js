50b37d153fa65176a6a4a8ab150f872cb40fbe6f
Dvachini <izzuddinazman5@gmail.com>
2022-08-04 09:07:44 +0800

    generate klinik

--- client/src/user/components/UserGenerateKlinik.js
+++ client/src/user/components/UserGenerateKlinik.js
@@ -79,92 +79,115 @@ export default function UserGenerateKlinik() {
 
   return (
     <>
-      <div className='display-flex'>
+      <div className='p-2'>
+        <h1 className='font-bold text-base '>Generate Reten</h1>
         <form onSubmit={handleJana}>
-          <div className='grid grid-cols-4 grid-flow-col'>
-            <strong>Reten: </strong>
-            <select
-              required
-              name='jenisReten'
-              id='jenisReten'
-              onChange={(e) => setJenisReten(e.target.value)}
-            >
-              <option value=''>Sila pilih reten</option>
-              <option value='BEGIN'>BEGIN 01/2020</option>
-              <option value='PGS203'>PGS203 (Pind. 1/2021)</option>
-              <option value='CPPC1'>CPPC 1</option>
-              <option value='CPPC2'>CPPC 2</option>
-              <option value='PG201A'>PG201A</option>
-              <option value='MMI1'>BORANG MMI/1</option>
-              <option value='FV'>FV</option>
-              <option value='PG201'>PG201</option>
-              <option value='GIS'>GIS</option>
-              <option value='PERSIS1'>PERSiS-1</option>
-              <option value='PPIM03'>PPIM 03-2020</option>
-              <option value='PPIM04'>PPIM 04</option>
-              <option value='PPIM05'>PPIM 05-2020</option>
-              <option value='PPKPS'>PPKPS</option>
-              <option value='MGH'>MGH</option>
-            </select>
-            <strong>Sekolah: </strong>
-            <select
-              value={pilihanSekolah}
-              onChange={(e) => {
-                setPilihanSekolah(e.target.value);
-              }}
-              className='capitalize outline outline-1 outline-userBlack'
-            >
-              <option value=''>Sila pilih..</option>
-              {namaSekolahs.map((singleNamaSekolah, index) => {
-                return (
-                  <option
-                    value={singleNamaSekolah}
-                    key={index}
-                    className='capitalize'
-                  >
-                    {singleNamaSekolah}
-                  </option>
-                );
-              })}
-            </select>
-            <strong>Daripada</strong>
-            <input
-              type='date'
-              name='startDate'
-              id='startDate'
-              onChange={(e) => setStartDate(e.target.value)}
-            />
-            <strong>Sehingga</strong>
-            <input
-              type='date'
-              name='endDate'
-              id='endDate'
-              onChange={(e) => setEndDate(e.target.value)}
-            />
-            <strong>Format: </strong>
-            <select
-              name='formatFile'
-              id='formatFile'
-              onChange={(e) => setFormatFile(e.target.value)}
-            >
-              <option value=''>Sila pilih format file</option>
-              <option value='xlsx'>Excel</option>
-              <option value='pdf'>PDF</option>
-              <option value='yeezus'>Holy Grail</option>
-            </select>
-          </div>
-          <br />
-          <div>
-            {/* <button className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 mr-2 hover:bg-user1 transition-all'>
+          <div className='grid grid-cols-4 gap-3'>
+            <div className='flex flex-row'>
+              <p className='flex items-center flex-row pl-5 font-semibold'>
+                reten :{' '}
+              </p>
+              <select
+                required
+                name='jenisReten'
+                id='jenisReten'
+                onChange={(e) => setJenisReten(e.target.value)}
+                className='outline outline-1 outline-userBlack w-32 m-3 text-sm font-m'
+              >
+                <option value=''>Sila pilih reten</option>
+                <option value='BEGIN'>BEGIN 01/2020</option>
+                <option value='PGS203'>PGS203 (Pind. 1/2021)</option>
+                <option value='CPPC1'>CPPC 1</option>
+                <option value='CPPC2'>CPPC 2</option>
+                <option value='PG201A'>PG201A</option>
+                <option value='MMI1'>BORANG MMI/1</option>
+                <option value='FV'>FV</option>
+                <option value='PG201'>PG201</option>
+                <option value='GIS'>GIS</option>
+                <option value='PERSIS1'>PERSiS-1</option>
+                <option value='PPIM03'>PPIM 03-2020</option>
+                <option value='PPIM04'>PPIM 04</option>
+                <option value='PPIM05'>PPIM 05-2020</option>
+                <option value='PPKPS'>PPKPS</option>
+                <option value='MGH'>MGH</option>
+              </select>
+            </div>
+            <div className='flex flex-row'>
+              <p className='flex items-center flex-row pl-5 font-semibold'>
+                Sekolah :{' '}
+              </p>
+              <select
+                value={pilihanSekolah}
+                onChange={(e) => {
+                  setPilihanSekolah(e.target.value);
+                }}
+                className='outline outline-1 outline-userBlack w-32 m-3 text-sm font-m'
+              >
+                <option value=''>Sila pilih..</option>
+                {namaSekolahs.map((singleNamaSekolah, index) => {
+                  return (
+                    <option
+                      value={singleNamaSekolah}
+                      key={index}
+                      className='capitalize'
+                    >
+                      {singleNamaSekolah}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className='flex flex-row'>
+              <p className='flex items-center flex-row pl-5 font-semibold'>
+                Daripada :{' '}
+              </p>
+              <input
+                type='date'
+                name='startDate'
+                id='startDate'
+                onChange={(e) => setStartDate(e.target.value)}
+                className='outline outline-1 outline-userBlack w-32 m-3 text-sm font-m'
+              />
+            </div>
+            <div className='flex flex-row'>
+              <p className='flex items-center flex-row pl-5 font-semibold'>
+                Sehingga :{' '}
+              </p>
+              <input
+                type='date'
+                name='endDate'
+                id='endDate'
+                onChange={(e) => setEndDate(e.target.value)}
+                className='outline outline-1 outline-userBlack w-32 m-3 text-sm font-m'
+              />
+            </div>
+            <div className='col-span-4'>
+              {/* <button className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 mr-2 hover:bg-user1 transition-all'>
               cetak
             </button> */}
-            <button
-              className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 ml-2 hover:bg-user1 transition-all'
-              type='submit'
-            >
-              jana
-            </button>
+              <div className=''>
+                <p className='items-center pl-5 font-semibold'>Format : </p>
+                <select
+                  name='formatFile'
+                  id='formatFile'
+                  onChange={(e) => setFormatFile(e.target.value)}
+                  className='outline outline-1 outline-userBlack w- m-3 text-sm font-m'
+                >
+                  <option value=''>Sila pilih format file</option>
+                  <option value='xlsx'>Excel</option>
+                  <option value='pdf'>PDF</option>
+                  <option value='yeezus'>Holy Grail</option>
+                </select>
+              </div>
+              <button
+                className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 ml-2 hover:bg-user1 transition-all'
+                type='submit'
+              >
+                jana
+              </button>
+            </div>
           </div>
+
           <div className='p-2 items-center mt-2'>
             <iframe
               className='w-full'
