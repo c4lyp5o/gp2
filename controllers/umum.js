01045a4cd32aa499fec00bbeb1163f7f26fc709d
noct <ezy_95@outlook.com>
2022-08-28 22:17:49 +0800

    umum decrypt ic

--- controllers/umum.js
+++ controllers/umum.js
@@ -1,5 +1,7 @@
 const Umum = require('../models/Umum');
+const cryptoJs = require('crypto-js');
 
+// GET /:id
 const getSinglePersonUmum = async (req, res) => {
   if (req.user.accountType !== 'kpUser') {
     return res.status(401).json({ msg: 'Unauthorized' });
@@ -15,9 +17,17 @@ const getSinglePersonUmum = async (req, res) => {
     return res.status(404).json({ msg: `No person with id ${personUmumId}` });
   }
 
+  // decrypt
+  const decryptedIc = cryptoJs.AES.decrypt(
+    singlePersonUmum.ic,
+    process.env.CRYPTO_JS_SECRET
+  ).toString(cryptoJs.enc.Utf8);
+  singlePersonUmum.ic = decryptedIc;
+
   res.status(200).json({ singlePersonUmum });
 };
 
+// PATCH /:id
 const updatePersonUmum = async (req, res) => {
   if (req.user.accountType !== 'kpUser') {
     return res.status(401).json({ msg: 'Unauthorized' });
@@ -31,6 +41,13 @@ const updatePersonUmum = async (req, res) => {
   req.body.createdByNegeri = req.user.negeri;
   req.body.createdByDaerah = req.user.daerah;
   req.body.createdByKp = req.user.kp;
+
+  // encrypt
+  const encryptedIc = cryptoJs.AES.encrypt(
+    req.body.ic,
+    process.env.CRYPTO_JS_SECRET
+  ).toString();
+  req.body.ic = encryptedIc;
 
   const updatedSinglePersonUmum = await Umum.findOneAndUpdate(
     { _id: personUmumId },
@@ -71,6 +88,15 @@ const queryPersonUmum = async (req, res) => {
   }
 
   const umumResultQuery = await Umum.find(queryObject);
+
+  // decrypt
+  umumResultQuery.forEach((p) => {
+    const decryptedIc = cryptoJs.AES.decrypt(
+      p.ic,
+      process.env.CRYPTO_JS_SECRET
+    ).toString(cryptoJs.enc.Utf8);
+    p.ic = decryptedIc;
+  });
 
   res.status(200).json({ umumResultQuery });
 };
