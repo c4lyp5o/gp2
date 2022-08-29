90443c8a4146edf9e6c018f8c69362011d3dd209
noct <ezy_95@outlook.com>
2022-08-29 15:12:31 +0800

    user login, afterlogin, navbar, loggedin, header & footer change from sm to lg

--- client/src/user/components/UserFooter.js
+++ client/src/user/components/UserFooter.js
@@ -1,12 +1,12 @@
 function UserFooter() {
   return (
     <div className='absolute bottom-0 left-0 right-0 grid grid-cols-2 bg-user3 uppercase'>
-      <span className='text-left ml-5 my-1 text-xs sm:text-sm'>
-        hak cipta kkm
-      </span>
-      <span className='text-right mr-1 sm:mr-5 my-1 text-xs sm:text-sm truncate'>
-        helpdesk : emel / phone / link ke borang pertanyaan
-      </span>
+      <p className='text-left ml-1 my-1 text-xs'>hak cipta kkm</p>
+      <p className='text-right mr-1 my-1 text-xs whitespace-nowrap overflow-x-auto'>
+        helpdesk:
+        <a href='#'> emel</a> /<a href='#'> phone</a> /
+        <a href='#'> link ke borang pertanyaan</a>
+      </p>
     </div>
   );
 }
