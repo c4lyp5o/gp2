a0ee467b687d27399acd2fac5e5fabf869c0b4bc
c4lyp5o <calypso.hellhammer@gmail.com>
2022-09-11 20:40:05 +0800

    eat diet pills

--- client/src/admin/components/old/AdminCenterStageLoggedIn.js
+++
@@ -1,9 +0,0 @@
-function AdminCenterStageLoggedIn({ children }) {
-  return (
-    <>
-      <div className='h-full p-5 overflow-y-auto'>{children}</div>
-    </>
-  );
-}
-
-export default AdminCenterStageLoggedIn;
