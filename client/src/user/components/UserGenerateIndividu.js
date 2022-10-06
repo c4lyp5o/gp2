acc448c26b7541074db2f9c3153cc9140999cfe4
Dvachini <izzuddinazman5@gmail.com>
2022-10-06 15:52:08 +0800

    generate kasi paduu

--- client/src/user/components/UserGenerateIndividu.js
+++ client/src/user/components/UserGenerateIndividu.js
@@ -44,74 +44,104 @@ export default function UserGenerateIndividu() {
 
   return (
     <>
-      <div className='display-flex'>
+      <div className='p-2'>
+        <h1 className='font-bold text-lg text-user1 '>Penjanaan Laporan</h1>
         <form action=''>
-          <div className='grid grid-cols-4 grid-flow-col'>
-            <strong>Reten: </strong>
-            <select
-              name='jenisReten'
-              id='jenisReten'
-              onChange={(e) => setJenisReten(e.target.value)}
-              defaultValue='plsSlct'
-            >
-              <option value='plsSlct'>Sila pilih reten</option>
-              <option value='BEGIN'>BEGIN 01/2020</option>
-              <option value='PGS203'>PGS203 (Pind. 1/2021)</option>
-              <option value='CPPC1'>CPPC 1</option>
-              <option value='CPPC2'>CPPC 2</option>
-              <option value='PG201A'>PG201A</option>
-              <option value='MMI1'>BORANG MMI/1</option>
-              <option value='FV'>FV</option>
-              <option value='PG201'>PG201</option>
-              <option value='GIS'>GIS</option>
-              <option value='PERSIS1'>PERSiS-1</option>
-              <option value='PPIM03'>PPIM 03-2020</option>
-              <option value='PPIM04'>PPIM 04</option>
-              <option value='PPIM05'>PPIM 05-2020</option>
-              <option value='PPKPS'>PPKPS</option>
-              <option value='MGH'>MGH</option>
-            </select>
-            <strong>Daripada</strong>
-            <input
-              type='date'
-              name='startDate'
-              id='startDate'
-              onChange={(e) => setStartDate(e.target.value)}
-            />
-            <strong>Sehingga</strong>
-            <input
-              type='date'
-              name='endDate'
-              id='endDate'
-              onChange={(e) => setEndDate(e.target.value)}
-            />
-            {/* <strong>Format: </strong>
-            <select
-              name='formatFile'
-              id='formatFile'
-              onChange={(e) => setFormatFile(e.target.value)}
-              defaultValue='plsSlct'
-            >
-              <option value='plsSlct'>Sila pilih format file</option>
-              <option value='xlsx'>Excel</option>
-              <option value='pdf'>PDF</option>
-              <option value='yeezus'>Holy Grail</option>
-            </select> */}
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
+            <div className='px-3 py-1'>
+              <label
+                htmlFor='jenisReten'
+                className='text-sm font-semibold text-user1 flex flex-row items-center p-2'
+              >
+                Laporan:
+              </label>
+              <select
+                name='jenisReten'
+                id='jenisReten'
+                onChange={(e) => setJenisReten(e.target.value)}
+                className='appearance-none w-full px-2 py-1 text-sm text-user1 border border-user1 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-user1 focus:border-transparent'
+                defaultValue='plsSlct'
+              >
+                <option value='plsSlct'>Sila pilih reten</option>
+                <option value='BEGIN'>BEGIN 01/2020</option>
+                <option value='PGS203'>PGS203 (Pind. 1/2021)</option>
+                <option value='CPPC1'>CPPC 1</option>
+                <option value='CPPC2'>CPPC 2</option>
+                <option value='PG201A'>PG201A</option>
+                <option value='MMI1'>BORANG MMI/1</option>
+                <option value='FV'>FV</option>
+                <option value='PG201'>PG201</option>
+                <option value='GIS'>GIS</option>
+                <option value='PERSIS1'>PERSiS-1</option>
+                <option value='PPIM03'>PPIM 03-2020</option>
+                <option value='PPIM04'>PPIM 04</option>
+                <option value='PPIM05'>PPIM 05-2020</option>
+                <option value='PPKPS'>PPKPS</option>
+                <option value='MGH'>MGH</option>
+              </select>
+            </div>
+            <div className='px-3 py-1'>
+              <label
+                htmlFor='startDate'
+                className='text-sm font-semibold text-user1 flex flex-row items-center p-2'
+              >
+                Daripada
+              </label>
+              <input
+                type='date'
+                name='startDate'
+                id='startDate'
+                onChange={(e) => setStartDate(e.target.value)}
+                className='appearance-none w-full px-2 py-1 text-sm text-user1 border border-user1 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-user1 focus:border-transparent'
+              />
+            </div>
+            <div className='px-3 py-1'>
+              <label
+                htmlFor='endDate'
+                className='text-sm font-semibold text-user1 flex flex-row items-center p-2'
+              >
+                Sehingga
+              </label>
+              <input
+                type='date'
+                name='endDate'
+                id='endDate'
+                onChange={(e) => setEndDate(e.target.value)}
+                className='appearance-none w-full px-2 py-1 text-sm text-user1 border border-user1 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-user1 focus:border-transparent'
+              />
+            </div>
           </div>
-          <br />
-          <div>
-            <button className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 mr-2 hover:bg-user1 transition-all'>
+          <div className='grid grid-cols-3 '>
+            {/* <button className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 mr-2 hover:bg-user1 transition-all'>
               cetak
-            </button>
+            </button> */}
+            <div className='px-3 py-1 col-start-2'>
+              <label
+                htmlFor='formatFile'
+                className='text-sm font-semibold text-user1 flex flex-row items-center p-2'
+              >
+                Format:
+              </label>
+              <select
+                required
+                name='formatFile'
+                id='formatFile'
+                onChange={(e) => setFormatFile(e.target.value)}
+                className='appearance-none w-full px-2 py-1 text-sm text-user1 border border-user1 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-user1 focus:border-transparent'
+              >
+                <option value=''>Sila pilih format file</option>
+                <option value='xlsx'>Excel</option>
+                {/* <option value='pdf'>PDF</option> */}
+              </select>
+            </div>
             <button
-              className='capitalize bg-user3 text-userWhite rounded-md shadow-xl p-2 ml-2 hover:bg-user1 transition-all'
-              onClick={handleJana}
+              className='capitalize bg-user3 text-userWhite rounded-md shadow-xl px-3 py-2 mx-3 my-2 hover:bg-user1 transition-all col-start-2'
+              type='submit'
             >
               jana
             </button>
           </div>
-          <p>{errorMsg}</p>
-          <div className='p-2 items-center mt-2'>
+          {/* <div className='p-2 items-center mt-2'>
             <iframe
               className='w-full'
               height='400'
@@ -119,7 +149,7 @@ export default function UserGenerateIndividu() {
               src=''
               frameBorder='0'
             ></iframe>
-          </div>
+          </div> */}
         </form>
       </div>
     </>
