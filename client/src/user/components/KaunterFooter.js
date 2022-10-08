487e35cd4c7ed05e3cf80c83e6a0b64dfe6f0e1f
Dvachini <izzuddinazman5@gmail.com>
2022-10-08 09:09:06 +0800

    pendaftaran mobile part1

--- client/src/user/components/KaunterFooter.js
+++ client/src/user/components/KaunterFooter.js
@@ -1,8 +1,8 @@
 function UserFooter() {
   return (
     <div className='absolute bottom-0 left-0 right-0 grid grid-cols-2 bg-kaunter1 uppercase'>
-      <span className='text-left ml-5 my-1'>hak cipta kkm</span>
-      <span className='text-right mr-5 my-1'>
+      <span className='text-left ml-5 my-1 text-xs'>hak cipta kkm</span>
+      <span className='text-right mr-5 my-1 text-xs whitespace-nowrap overflow-x-auto'>
         <a
           className='text-admin2 underline'
           href='https://forms.gle/v9P7w9qweTX86Nxn8'
--- client/src/user/components/KaunterHeaderLoggedIn.js
+++ client/src/user/components/KaunterHeaderLoggedIn.js
@@ -1,6 +1,6 @@
 function KaunterHeaderLoggedIn({ namaKlinik, logout }) {
   return (
-    <div className='absolute top-10 right-5 flex w-auto h-10 items-center justify-center capitalize text-kaunterWhite text-xs'>
+    <div className='absolute top-10 right-5 hidden lg:flex w-auto h-10 items-center justify-center capitalize text-kaunterWhite text-xs'>
       <div className='m-3 space-y-1 text-right pr-2'>
         <p className='w-96 text-sm leading-3'>
           <b>pendaftaran : </b>
