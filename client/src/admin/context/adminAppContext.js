004005e327a928cce71a194aed8f4e8a20b70953
c4lyp5o <calypso.hellhammer@gmail.com>
2022-10-11 12:34:32 +0800

    feat: added klinik details page for superadmin and reten generation right at the door, added latest pg206 and pg207, added klinik data fetcher in context

--- client/src/admin/context/adminAppContext.js
+++ client/src/admin/context/adminAppContext.js
@@ -54,13 +54,23 @@ function AdminAppProvider({ children }) {
 
   // hq functions
 
-  const getAllNegeriAndDaerah = async (accountType) => {
+  const getAllNegeriAndDaerah = async () => {
     let response = await axios.post(`/api/v1/superadmin/newroute`, {
       apiKey: process.env.REACT_APP_API_KEY,
       main: 'HqCenter',
       Fn: 'read',
-      accountType: accountType,
       token: getTokenized(),
+    });
+    return response;
+  };
+
+  const getKlinikData = async (id) => {
+    let response = await axios.post(`/api/v1/superadmin/newroute`, {
+      apiKey: process.env.REACT_APP_API_KEY,
+      main: 'HqCenter',
+      Fn: 'readOne',
+      token: getTokenized(),
+      id,
     });
     return response;
   };
@@ -313,6 +323,7 @@ function AdminAppProvider({ children }) {
         checkUser,
         // hq
         getAllNegeriAndDaerah,
+        getKlinikData,
       }}
     >
       {children}
