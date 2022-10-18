9f037a5cadda55f7854876991e368b5683073c6f
Dvachini <izzuddinazman5@gmail.com>
2022-10-18 20:46:31 +0800

    admin-login

--- client/src/admin/context/countdownTimer.js
+++ client/src/admin/context/countdownTimer.js
@@ -12,10 +12,31 @@ const ExpiredNotice = () => {
 
 const ShowCounter = ({ minutes, seconds }) => {
   return (
-    <span className='flex justify-center bg-admin2 text-adminWhite text-sm font-medium px-2.5 py-1 rounded dark:bg-admin2 dark:text-adminWhite'>
-      Masa untuk log masuk:{<TimeDisplay value={minutes} />}:
-      {<TimeDisplay value={seconds} />}
-    </span>
+    <div className='flex text-center text-admin2 justify-center text-xs'>
+      <div className='flex gap-5'>
+        {/* <span className='flex justify-center bg-admin2 text-adminWhite text-sm font-medium px-2.5 py-1 rounded dark:bg-admin2 dark:text-adminWhite'>
+        Masa untuk log masuk:{<TimeDisplay value={minutes} />}:
+        {<TimeDisplay value={seconds} />}
+      </span> */}
+        <span className='flex items-center text-base font-mono'>
+          {' '}
+          Masa untuk log masuk:
+        </span>
+        <div className='flex flex-col items-center justify-center'>
+          <span className='font-mono text-xl'>
+            {<TimeDisplay value={minutes} />}
+          </span>
+          minit
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <span className='font-mono text-xl'>
+            {' '}
+            {<TimeDisplay value={seconds} />}
+          </span>
+          saat
+        </div>
+      </div>
+    </div>
   );
 };
 
