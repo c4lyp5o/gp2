9a4d55f129b946f06389fc32572e6410a8c08737
c4lyp5o <calypso.hellhammer@gmail.com>
2022-12-09 14:53:20 +0800

    WIP

--- routes/adminAPI.js
+++ routes/adminAPI.js
@@ -3,10 +3,17 @@ const express = require('express');
 const router = express.Router();
 
 // Controller
-const { getCipher, getData } = require('../controllers/adminAPI');
+const {
+  getCipher,
+  getData,
+  getDataRoute,
+  getDataKpRoute,
+} = require('../controllers/adminAPI');
 
 // Routes
 router.get('/getcipher', getCipher);
+router.get('/getdata', getDataRoute);
+router.get('/getkpdata', getDataKpRoute);
 router.post('/newroute', getData);
 
 module.exports = router;
