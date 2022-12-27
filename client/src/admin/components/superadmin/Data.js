3aa5df7f8c45bc83df17b057c7322ef0de51b38b
c4lyp5o <calypso@calypsocloud.one>
2022-12-27 12:35:11 +0800

    feat: added g2u and reflected to all pages that is using erkm, added kkiakd initial route, added kkia to navbar, adapted g2u data fetching

--- client/src/admin/components/superadmin/Data.js
+++ client/src/admin/components/superadmin/Data.js
@@ -26,6 +26,7 @@ export default function Data({ FType, kp }) {
 
   // short circuit
   const [showKlinik, setShowKlinik] = useState(false);
+  const [showKkia, setShowKkia] = useState(false);
   const [showOperators, setShowOperators] = useState(false);
   const [showFacilities, setShowFacilities] = useState(false);
   const [showEvent, setShowEvent] = useState(false);
@@ -60,11 +61,15 @@ export default function Data({ FType, kp }) {
       if (FType === 'kp') {
         setShowKlinik(true);
       }
+      if (FType === 'kkiakd') {
+        setShowKkia(true);
+      }
       if (FType === 'program') {
         setShowEvent(true);
       }
       if (
         FType !== 'kp' &&
+        FType !== 'kkiakd' &&
         FType !== 'jp' &&
         FType !== 'pp' &&
         FType !== 'program'
@@ -80,6 +85,7 @@ export default function Data({ FType, kp }) {
       setShowFacilities(false);
       setShowOperators(false);
       setShowKlinik(false);
+      setShowKkia(false);
       setShowEvent(false);
     };
   }, [FType, reload]);
@@ -216,6 +222,141 @@ export default function Data({ FType, kp }) {
                     </td>
                   </tr>
                 ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  function KKIAKD() {
+    if (data.length > 0) {
+      return (
+        <div className='flex flex-col items-center gap-5'>
+          <h1 className='text-3xl font-bold mt-10 mb-10'>
+            Senarai Klinik Pergigian Daerah {daerah}
+          </h1>
+          <div className='m-auto overflow-x-auto text-sm rounded-md h-min max-w-max'>
+            <table className='table-auto'>
+              <thead className='text-adminWhite bg-admin3'>
+                <tr>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Bil.
+                  </th>
+                  {/* <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Kod Fasiliti Gi-Ret 2.0
+                  </th> */}
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Nama KP
+                  </th>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Peranan KP
+                  </th>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Emel KP
+                  </th>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Akaun Pengguna KP
+                  </th>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Akaun Pendaftaran KP
+                  </th>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Status KP
+                  </th>
+                  <th className='px-1 py-1 outline outline-1 outline-offset-1'>
+                    Tindakan
+                  </th>
+                </tr>
+              </thead>
+              <tbody className='bg-admin4'>
+                {/* {data.map((kp, index) => (
+                  <tr key={kp._id}>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1'>
+                      {index + 1}
+                    </td>                   
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1'>
+                      {kp.kp}
+                    </td>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1'>
+                      {kp.statusRoleKlinik}
+                    </td>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1 normal-case'>
+                      {encryptEmail(kp.email)}
+                    </td>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1 normal-case'>
+                      <div id={index}>
+                        {showPassword[kp.username] === true
+                          ? kp.password
+                          : encryptPassword(kp.password)}
+                        <button
+                          className='ml-1'
+                          onClick={() => {
+                            setShowPassword({
+                              ...showPassword,
+                              [kp.username]: !showPassword[kp.username],
+                            });
+                          }}
+                        >
+                          <AiOutlineEye />
+                        </button>
+                      </div>
+                    </td>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1 normal-case'>
+                      <div id={index}>
+                        {showPassword[kp.kaunterUsername] === true
+                          ? kp.kaunterPassword
+                          : encryptPassword(kp.kaunterPassword)}
+                        <button
+                          className='ml-1'
+                          onClick={() =>
+                            setShowPassword({
+                              ...showPassword,
+                              [kp.kaunterUsername]:
+                                !showPassword[kp.kaunterUsername],
+                            })
+                          }
+                        >
+                          <AiOutlineEye />
+                        </button>
+                      </div>
+                    </td>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1'>
+                      {kp.statusPerkhidmatan === 'active' ? (
+                        <span className='bg-user7 text-adminWhite text-xs font-semibold px-1.5 py-0.5 rounded'>
+                          Aktif
+                        </span>
+                      ) : (
+                        <span className='bg-admin2 text-adminWhite text-xs font-semibold px-1.5 py-0.5 rounded whitespace-nowrap'>
+                          Tidak Aktif
+                        </span>
+                      )}
+                    </td>
+                    <td className='px-1 py-1 outline outline-1 outline-adminWhite outline-offset-1'>
+                      <button
+                        className='bg-admin3 relative top-0 right-0 p-1 w-20 rounded-md text-white shadow-xl m-1'
+                        onClick={() => {
+                          setShowEditModal(true);
+                          setId(kp._id);
+                        }}
+                      >
+                        Kemaskini
+                      </button>
+                      <button
+                        className='bg-admin3 relative top-0 right-0 p-1 w-20 rounded-md text-white shadow-xl m-1'
+                        id={kp._id}
+                        onClick={() => {
+                          setShowDeleteModal(true);
+                          setId(kp._id);
+                          setDeleteCandidate(kp.kp);
+                        }}
+                      >
+                        Hapus
+                      </button>
+                    </td>
+                  </tr>
+                ))} */}
               </tbody>
             </table>
           </div>
@@ -797,6 +938,7 @@ export default function Data({ FType, kp }) {
       <>
         {data.length === 0 && <NothingHereBoi FType={FType} />}
         {showKlinik && <Klinik />}
+        {showKkia && <KKIAKD />}
         {showOperators && <Pegawai />}
         {showFacilities && <Facility />}
         {showEvent && <Event />}
