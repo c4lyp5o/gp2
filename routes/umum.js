02efe2d6df2b04fb92e1f4a837c87ac2e84dacbe
noct <ezy_95@outlook.com>
2023-01-05 06:15:10 +0800

    fix: rework UserStatusHarian route to save network bandwith because it will pull patient for one whole year

--- routes/umum.js
+++ routes/umum.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllPersonUmum,
+  getAllPersonUmumStatus,
   getSinglePersonUmum,
   updatePersonUmum,
   retenSalahPersonUmum,
@@ -10,6 +11,7 @@ const {
 } = require('../controllers/umum');
 
 router.route('/').get(getAllPersonUmum);
+router.route('/status-harian').get(getAllPersonUmumStatus);
 router.route('/:id').get(getSinglePersonUmum).patch(updatePersonUmum);
 router.route('/salah/:id').patch(retenSalahPersonUmum);
 router.route('/delete/:id').patch(softDeletePersonUmum);
