99469e4f8b260a85222d2392df5f38d834b943a2
noct <ezy_95@outlook.com>
2023-02-27 16:13:48 +0800

    test: cypress test run

--- cypress/e2e/KaunterLogin.cy.js
+++ cypress/e2e/KaunterLogin.cy.js
@@ -13,7 +13,7 @@ describe('Login Pendaftaran', () => {
     cy.get('[data-cy="negeri"]').select('Johor');
     cy.get('[data-cy="daerah"]').select('Batu Pahat');
     cy.get('[data-cy="klinik"]').select('J01-002-02');
-    cy.get('[data-cy="password"]').type('123456');
+    cy.get('[data-cy="password"]').type('12345');
     cy.get('[data-cy="submit"]').click();
     cy.url().should('include', '/daftar');
   });
