5e8da7a4d8f05bad7c7c4f81d67d700523d8e591
c4lyp5o <calypso.hellhammer@gmail.com>
2023-03-09 00:02:32 +0800

    feat: bukak route custom single etl

--- routes/ETL.js
+++ routes/ETL.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   initiateETL,
   initiateCustomETL,
+  initiateCustomSingleETL,
 } = require('../controllers/ETLmanifest');
 
 router.get('/initiate', initiateETL);
 router.get('/initiatecustom', initiateCustomETL);
+router.get('/initiatecustomsingle', initiateCustomSingleETL);
 
 module.exports = router;
