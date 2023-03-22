b2fcb77bfb87e783e3511f1fcbed2fedb3ec2915
noct <ezy_95@outlook.com>
2023-03-22 14:54:55 +0800

    MIGRATE TO VITE: the config that got it fixed https://github.com/vitejs/vite/issues/2139#issuecomment-1399098579

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// Without it dynamic require is not possible in config file
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -10,4 +14,15 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      defaultIsModuleExports(id) {
+        const module = require(id);
+        if (module?.default) {
+          return false;
+        }
+        return 'auto';
+      },
+    },
+  },
 });
