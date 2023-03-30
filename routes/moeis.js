56a46c55b3da432c0da105f07a3590518845f024
noct <ezy_95@outlook.com>
2023-03-30 15:21:55 +0800

    feat: full blown integration with MOEIS defined in API route

--- routes/moeis.js
+++ routes/moeis.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { getSekolahMOEIS, getPelajarMOEIS } = require('../controllers/moeis');
+const {
+  getJPNMOEIS,
+  getSekolahMOEIS,
+  getSingleSekolahMOEIS,
+  getPelajarMOEIS,
+  getSinglePelajarMOEIS,
+} = require('../controllers/moeis');
 
+router.route('/jpn').get(getJPNMOEIS);
 router.route('/sekolah').get(getSekolahMOEIS);
+router.route('/singleSekolah').get(getSingleSekolahMOEIS);
 router.route('/pelajar').get(getPelajarMOEIS);
+router.route('/singlePelajar').get(getSinglePelajarMOEIS);
 
 module.exports = router;
