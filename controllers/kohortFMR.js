505f660e83ca778a7b31223d56916d4769df8c93
c4lyp5o <calypso@calypsocloud.one>
2023-04-11 12:50:15 +0800

    fix: buang dan tambah bbrp functions kohortFMR

--- controllers/kohortFMR.js
+++ controllers/kohortFMR.js
@@ -24,85 +24,160 @@ const getAllSekolahKohortFMR = async (req, res) => {
   res.status(200).json({ allSekolahKohortFMR });
 };
 
-const getAllD1StudentInSekolah = async (req, res) => {
+// GET /query
+const getAllD1StudentInRegisteredSekolah = async (req, res) => {
   logger.info(
-    `${req.method} ${req.url} [KohortFMRController] getAllD1StudentInSekolah called`
+    `${req.method} ${req.url} [KohortFMRController] getAllD1StudentInRegisteredSekolah called`
   );
 
   if (req.user.accountType !== 'kpUser') {
     return res.status(401).json({ msg: 'Unauthorized' });
   }
 
-  const { singleSekolahId } = req.params;
+  const { kodFasiliti } = req.user;
 
-  const singleSekolah = await Fasiliti.findOne({
-    kodSekolah: singleSekolahId,
+  const dataSekolahDenganPelajar = await Fasiliti.aggregate([
+    {
+      $match: {
+        kodFasilitiHandler: kodFasiliti,
+        statusFMRSekolah: 'ya',
+        jenisFasiliti: 'sekolah-rendah',
+      },
+    },
+    {
+      $facet: {
+        fasilitiSekolah: [
+          // {
+          //   $lookup: {
+          //     from: 'sekolahs',
+          //     let: {
+          //       kodSekolah: '$kodSekolah',
+          //     },
+          //     pipeline: [
+          //       {
+          //         $match: {
+          //           $expr: {
+          //             $and: [
+          //               {
+          //                 $eq: ['$kodSekolah', '$$kodSekolah'],
+          //               },
+          //             ],
+          //           },
+          //         },
+          //       },
+          //       {
+          //         $group: {
+          //           _id: null,
+          //           semuaTahun: {
+          //             $addToSet: '$tahun',
+          //           },
+          //           // semuaKelas: {
+          //           //   $addToSet: "$namaKelas"
+          //           // },
+          //         },
+          //       },
+          //       {
+          //         $project: {
+          //           _id: 0,
+          //           semuaTahun: 1,
+          //           // semuaKelas: 1,
+          //         },
+          //       },
+          //     ],
+          //     as: 'sedikitDetail',
+          //   },
+          // },
+          // {
+          //   $project: {
+          //     _id: 0,
+          //     idInstitusi: 1,
+          //     nama: 1,
+          //     kodSekolah: 1,
+          //     sekolahSelesaiReten: 1,
+          //     tarikhMulaSekolah: 1,
+          //     // tahunSekolah: '$sedikitDetail.semuaTahun',
+          //     // kelasSekolah: '$sedikitDetail.semuaKelas',
+          //   },
+          // },
+        ],
+        allD1PelajarSekolah: [
+          {
+            $lookup: {
+              from: 'sekolahs',
+              let: {
+                kodSekolah: '$kodSekolah',
+              },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $and: [
+                        {
+                          $eq: ['$kodSekolah', '$$kodSekolah'],
+                        },
+                        {
+                          $eq: ['$tahun', 'D1'],
+                        },
+                      ],
+                    },
+                  },
+                },
+                {
+                  $project: {
+                    _id: 1,
+                    nama: 1,
+                    noKp: 1,
+                    kodJantina: 1,
+                    kaum: 1,
+                    tahun: 1,
+                    namaKelas: 1,
+                    statusRawatan: 1,
+                  },
+                },
+              ],
+              as: 'sekolah',
+            },
+          },
+          {
+            $unwind: '$sekolah',
+          },
+          {
+            $project: {
+              _id: '$sekolah._id',
+              namaSekolah: '$nama',
+              kodSekolah: '$kodSekolah',
+              jenisFasiliti: 1,
+              nama: '$sekolah.nama',
+              noKp: '$sekolah.noKp',
+              kodJantina: '$sekolah.kodJantina',
+              kaum: '$sekolah.kaum',
+              tahun: '$sekolah.tahun',
+              namaKelas: '$sekolah.namaKelas',
+              statusRawatan: '$sekolah.statusRawatan',
+            },
+          },
+          {
+            $sort: {
+              nama: 1,
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    allD1PelajarSekolah: dataSekolahDenganPelajar[0].allD1PelajarSekolah,
+    fasilitiSekolah: dataSekolahDenganPelajar[0].fasilitiSekolah,
   });
 
-  if (!singleSekolah) {
-    return res.status(404).json({ msg: 'No sekolah found' });
-  }
-
-  const allD1Students = await Sekolah.find({
-    kodSekolah: singleSekolahId,
-    tahun: 'D1',
-  });
-
-  if (allD1Students.length < 1) {
-    return res.status(404).json({ msg: 'No students found' });
-  }
-
-  // const preprocessing = allStudents.map((student) => {
-  //   return {
-  //     createdByNegeri: negeri,
-  //     createdByDaerah: daerah,
-  //     createdByKodFasiliti: kodFasiliti,
-  //     createdByKp: kp,
-  //     // createdByMdcMdtb: student.createdByMdcMdtb,
-  //     // createdByUsername: student.createdByUsername,
-  //     nama: student.nama,
-  //     ic: student.noKp,
-  //     namaSekolah: student.namaSekolah,
-  //     kodSekolah: student.kodSekolah,
-  //     tahun: student.tahun,
-  //     kelas: student.namaKelas,
-  //     // noTelefon: student.noTelefon,
-  //     dalamPemantauanKohort: true,
-  //   };
+  // const singleSekolah = await Fasiliti.findOne({
+  //   kodSekolah: singleSekolahId,
   // });
 
-  // await KohortFMR.insertMany(preprocessing);
-  // await singleSekolah.updateOne({
-  //   statusFMRTelahDaftarDarjahSatu: true,
-  // });
-  // console.log(allD1Students);
-
-  res.status(200).json({ allD1Students });
-};
-
-const getAllD1Student = async (req, res) => {
-  logger.info(
-    `${req.method} ${req.url} [KohortFMRController] getAllD1Student called`
-  );
-
-  if (req.user.accountType !== 'kpUser') {
-    return res.status(401).json({ msg: 'Unauthorized' });
-  }
-
-  const { negeri, daerah, kodFasiliti, kp } = req.user;
-
-  const allSekolah = await Fasiliti.find({
-    kodFasilitiHandler: kodFasiliti,
-    jenisFasiliti: 'sekolah-rendah',
-  })
-    .select('kodSekolah')
-    .lean();
-
-  if (!allSekolah) {
-    return res.status(404).json({ msg: 'No sekolah found' });
-  }
-
-  console.log(allSekolah.flatMap((x) => x.kodSekolah));
+  // if (!singleSekolah) {
+  //   return res.status(404).json({ msg: 'No sekolah found' });
+  // }
 
   // const allD1Students = await Sekolah.find({
   //   kodSekolah: singleSekolahId,
@@ -138,12 +213,13 @@ const getAllD1Student = async (req, res) => {
   // });
   // console.log(allD1Students);
 
-  res.status(200).json([]);
+  // res.status(200).json({ allD1Students });
 };
 
-const registerSekolahFMR = async (req, res) => {
+// PATCH /daftar-kumuran
+const daftarKumuranFMR = async (req, res) => {
   logger.info(
-    `${req.method} ${req.url} [KohortFMRController] registerSingleSekolahFMR called`
+    `${req.method} ${req.url} [KohortFMRController] daftarKumuran called`
   );
 
   if (req.user.accountType !== 'kpUser') {
@@ -151,122 +227,30 @@ const registerSekolahFMR = async (req, res) => {
   }
 
   const { negeri, daerah, kodFasiliti, kp } = req.user;
-  const { kodSekolah } = req.query;
 
-  const singleSekolah = await Fasiliti.findOne({
-    kodSekolah,
+  const { muridKumuran, startKumuran } = req.body;
+
+  console.log(muridKumuran, startKumuran);
+
+  muridKumuran.forEach(async (murid) => {
+    const singlePersonKohortFMR = await KohortFMR.findOne({
+      nomborId: murid.nomborId,
+    });
+
+    const updatedSinglePersonKohortFMR = await KohortFMR.findOneAndUpdate(
+      {
+        nomborId: murid.nomborId,
+      },
+      {
+        $push: {
+          tarikhKumuranMurid: startKumuran,
+        },
+      },
+      { new: true }
+    );
   });
 
-  if (!singleSekolah) {
-    return res.status(404).json({ msg: 'No sekolah found' });
-  }
-
-  const allD1Students = await Sekolah.find({
-    kodSekolah,
-    tahun: 'D1',
-  });
-
-  if (allD1Students.length < 1) {
-    return res.status(404).json({ msg: 'No students found' });
-  }
-
-  // const preprocessing = allStudents.map((student) => {
-  //   return {
-  //     createdByNegeri: negeri,
-  //     createdByDaerah: daerah,
-  //     createdByKodFasiliti: kodFasiliti,
-  //     createdByKp: kp,
-  //     // createdByMdcMdtb: student.createdByMdcMdtb,
-  //     // createdByUsername: student.createdByUsername,
-  //     nama: student.nama,
-  //     ic: student.noKp,
-  //     namaSekolah: student.namaSekolah,
-  //     kodSekolah: student.kodSekolah,
-  //     tahun: student.tahun,
-  //     kelas: student.namaKelas,
-  //     // noTelefon: student.noTelefon,
-  //     dalamPemantauanKohort: true,
-  //   };
-  // });
-
-  // await KohortFMR.insertMany(preprocessing);
-  // await singleSekolah.updateOne({
-  //   statusFMRTelahDaftarDarjahSatu: true,
-  // });
-
-  res.status(200).json({ allD1Students });
-};
-
-const getAllPersonKohortFMR = async (req, res) => {
-  logger.info(
-    `${req.method} ${req.url} [KohortFMRController] getAllPersonKohortFMR called`
-  );
-
-  if (req.user.accountType !== 'kpUser') {
-    return res.status(401).json({ msg: 'Unauthorized' });
-  }
-
-  const { kodFasiliti } = req.user;
-
-  const allPersonKohortFMR = await KohortFMR.find({
-    createdByKodFasiliti: kodFasiliti,
-  });
-
-  console.log(allPersonKohortFMR);
-
-  res.status(200).json({ allPersonKohortFMR });
-};
-
-// GET /:personKohortFMRId
-const getPersonKohortFMR = async (req, res) => {
-  logger.info(
-    `${req.method} ${req.url} [KohortFMRController] getSinglePersonKohort called`
-  );
-  if (req.user.accountType !== 'kpUser') {
-    return res.status(401).json({ msg: 'Unauthorized' });
-  }
-
-  const { personKohortFMRId } = req.params;
-
-  const singlePersonKohortFMR = await KohortFMR.findOne({
-    _id: personKohortFMRId,
-  });
-
-  if (!personKohortFMRId) {
-    return res
-      .status(404)
-      .json({ msg: `No person with id ${personKohortFMRId}` });
-  }
-
-  res.status(201).json({ singlePersonKohortFMR });
-};
-
-// PATCH /:personKohortFMRId
-const updatePersonKohortFMR = async (req, res) => {
-  logger.info(
-    `${req.method} ${req.url} [KohortFMRController] updatePersonKohort called`
-  );
-  if (req.user.accountType !== 'kpUser') {
-    return res.status(401).json({ msg: 'Unauthorized' });
-  }
-
-  const { personKohortFMRId } = req.params;
-
-  const updatedSinglePersonKohortFMR = await KohortFMR.findOneAndUpdate(
-    {
-      _id: req.params.personKohortFMRId,
-    },
-    req.body,
-    { new: true }
-  );
-
-  if (!updatedSinglePersonKohortFMR) {
-    return res
-      .status(404)
-      .json({ msg: `No person with id ${personKohortFMRId}` });
-  }
-
-  res.status(200).json({ updatedSinglePersonKohortFMR });
+  res.status(200).json({ msg: 'OK' });
 };
 
 // DELETE /:personKohortFMRId
@@ -353,12 +337,8 @@ const queryPersonKohortFMR = async (req, res) => {
 
 module.exports = {
   getAllSekolahKohortFMR,
-  getAllD1Student,
-  getAllD1StudentInSekolah,
-  getAllPersonKohortFMR,
-  registerSekolahFMR,
-  getPersonKohortFMR,
-  updatePersonKohortFMR,
+  getAllD1StudentInRegisteredSekolah,
+  daftarKumuranFMR,
   deletePersonKohortFMR,
   queryPersonKohortFMR,
 };
