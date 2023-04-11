e199245d4f7937f7618911844ad7ef0c3ddd22d5
c4lyp5o <calypso@calypsocloud.one>
2023-04-11 12:50:33 +0800

    fix: buang dan tambah bbrp route kohortFMR

--- routes/kohortFMR.js
+++ routes/kohortFMR.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllSekolahKohortFMR,
-  getAllD1Student,
-  registerSekolahFMR,
+  // getAllD1Student,
+  getAllD1StudentInRegisteredSekolah,
+  // registerSekolahFMR,
+  daftarKumuranFMR,
   getAllPersonKohortFMR,
   getPersonKohortFMR,
   updatePersonKohortFMR,
@@ -12,7 +14,8 @@ const {
 } = require('../controllers/kohortFMR');
 
 router.route('/').get(getAllSekolahKohortFMR);
-router.route('/query/').get(getAllD1Student);
+router.route('/query').get(getAllD1StudentInRegisteredSekolah);
+router.route('/daftar-kumuran').post(daftarKumuranFMR);
 // router.route('/daftar/:singleSekolahId').get(registerSekolahFMR);
 // router.route('/telah-daftar/').get(getAllPersonKohortFMR);
 // router
