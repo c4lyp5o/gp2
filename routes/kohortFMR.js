2c9f131a48536c45b6a4bf1ae21c8984e6a9581f
c4lyp5o <calypso@calypsocloud.one>
2023-04-14 17:06:36 +0800

    fix: reroute route fmr

--- routes/kohortFMR.js
+++ routes/kohortFMR.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getAllSekolahKohortFMR,
-  // getAllD1Student,
-  getAllD1StudentInRegisteredSekolah,
-  // registerSekolahFMR,
+  getAllSekolahFMR,
+  getAllD1StudentInSekolahFMR,
+  getAllD1StudentInSingleSekolahFMR,
+  getMuridDalamKohortFMR,
+  daftarMuridMasukKohortFMR,
   daftarKumuranFMR,
-  getAllPersonKohortFMR,
-  getPersonKohortFMR,
-  updatePersonKohortFMR,
   deletePersonKohortFMR,
-  queryPersonKohortFMR,
 } = require('../controllers/kohortFMR');
 
-router.route('/').get(getAllSekolahKohortFMR);
-router.route('/query').get(getAllD1StudentInRegisteredSekolah);
-router.route('/daftar-kumuran').post(daftarKumuranFMR);
+router.route('/').get(getAllSekolahFMR);
+router.route('/semua-murid').get(getAllD1StudentInSekolahFMR);
+router
+  .route('/pilih-murid/:singleSekolahFMRId')
+  .get(getAllD1StudentInSingleSekolahFMR);
+router.route('/daftar-kumuran').patch(daftarKumuranFMR);
+router.route('/murid-kohort').get(getMuridDalamKohortFMR);
+router.route('/daftar-murid').post(daftarMuridMasukKohortFMR);
 // router.route('/daftar/:singleSekolahId').get(registerSekolahFMR);
 // router.route('/telah-daftar/').get(getAllPersonKohortFMR);
 // router
