ae09ee1a1870a0ccf9686b6bbee573192634c896
c4lyp5o <calypso@calypsocloud.one>
2023-04-17 17:15:11 +0800

    fix: betulkan carian sikit2

--- client/src/user/components/kohort/FMR/CarianFMR.jsx
+++ client/src/user/components/kohort/FMR/CarianFMR.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { BsTrash } from 'react-icons/bs';
 import axios from 'axios';
 import moment from 'moment';
 import _ from 'lodash';
@@ -11,10 +12,19 @@ export default function UserCarianPromosi() {
     useGlobalUserAppContext();
 
   const [isLoading, setIsLoading] = useState(true);
-  const [allPersonInKohort, setAllPersonInKohort] = useState('');
+
+  const [allMuridKohort, setAllMuridKohort] = useState('');
   const [allSekolahKohortFMR, setAllSekolahKohortFMR] = useState([]);
-  const [pilihanSekolah, setPilihanSekolah] = useState('');
-  const [pilihanKelas, setPilihanKelas] = useState('');
+  const [allKelasBasedOnSekolah, setAllKelasBasedOnSekolah] = useState([]);
+  const [allTahunKohort, setAllTahunKohort] = useState([]);
+
+  // const [pilihanSekolah, setPilihanSekolah] = useState('');
+  // const [pilihanKelas, setPilihanKelas] = useState('');
+  const [selectedSekolah, setSelectedSekolah] = useState('');
+  const [selectedKelas, setSelectedKelas] = useState('');
+  const [selectedTahunKohort, setSelectedTahunKohort] = useState('');
+  const [selectedMurid, setSelectedMurid] = useState([]);
+
   const [tahunMulaKumuran, setTahunMulaKumuran] = useState('');
   const [modalHapusPromosi, setModalHapusPromosi] = useState(false);
 
@@ -22,21 +32,25 @@ export default function UserCarianPromosi() {
   const [refetch, setRefetch] = useState(false);
 
   useEffect(() => {
-    const fetchAllPersonInKohortFMR = async (e) => {
+    const fetchAllMuridFMR = async (e) => {
       try {
-        const { data } = await axios.get(`/api/v1/kohort/fmr/telah-daftar`, {
+        const { data } = await axios.get(`/api/v1/kohort/fmr/murid-kohort`, {
           headers: {
             Authorization: `Bearer ${
               reliefUserToken ? reliefUserToken : userToken
             }`,
           },
         });
-        setAllPersonInKohort(data.allPersonKohortFMR);
+        setAllMuridKohort(data.muridDalamKohortFMR);
         const nama2Sekolah = _.uniq(
-          data.allPersonKohortFMR.map((x) => x.namaSekolah)
+          data.muridDalamKohortFMR.map((x) => x.namaSekolah)
+        );
+        const nama2TahunKohort = _.uniq(
+          data.muridDalamKohortFMR.map((x) => x.tahunKohortFMR)
         );
         console.log(nama2Sekolah);
         setAllSekolahKohortFMR(nama2Sekolah);
+        setAllTahunKohort(nama2TahunKohort);
         setIsLoading(false);
       } catch (error) {
         console.log(error);
@@ -45,7 +59,7 @@ export default function UserCarianPromosi() {
         // );
       }
     };
-    fetchAllPersonInKohortFMR();
+    fetchAllMuridFMR();
   }, []);
 
   //   useEffect(() => {
@@ -72,34 +86,6 @@ export default function UserCarianPromosi() {
   //     };
   //     refetchDataOnDelete();
   //   }, [refetch]);
-
-  //   useEffect(() => {
-  //     const fetchAllProgramPromosi = async () => {
-  //       try {
-  //         setIsLoading(true);
-  //         const { data } = await axios.get('/api/v1/promosi', {
-  //           headers: {
-  //             Authorization: `Bearer ${
-  //               reliefUserToken ? reliefUserToken : userToken
-  //             }`,
-  //           },
-  //         });
-  //         const withoutDuplicateJenisProgram = data.allProgramPromosi.map(
-  //           (a) => a.jenisProgram
-  //         );
-  //         const withoutDuplicate = [...new Set(withoutDuplicateJenisProgram)];
-  //         setJenisProgram(withoutDuplicate);
-  //         setAllProgramPromosi(data.allProgramPromosi);
-  //         setIsLoading(false);
-  //       } catch (error) {
-  //         console.log(error);
-  //         // toast.error(
-  //         //   'Uh oh, server kita sedang mengalami masalah. Sila berhubung dengan team Gi-Ret 2.0 untuk bantuan. Kod: user-promosi-fetchAllProgramPromosi'
-  //         // );
-  //       }
-  //     };
-  //     fetchAllProgramPromosi();
-  //   }, [reliefUserToken, userToken]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -138,79 +124,120 @@ export default function UserCarianPromosi() {
     setQueryResult([]);
   };
 
+  useEffect(() => {
+    if (selectedSekolah === '') {
+      console.log('resetting');
+      setSelectedKelas('');
+    } else {
+      const allKelasBasedOnSekolah = _.uniq(
+        allMuridKohort
+          .filter((murid) => murid.namaSekolah === selectedSekolah)
+          .map((murid) => murid.namaKelas)
+      );
+      setAllKelasBasedOnSekolah(allKelasBasedOnSekolah);
+      setSelectedMurid(
+        allMuridKohort.filter((murid) => murid.namaSekolah === selectedSekolah)
+      );
+    }
+  }, [selectedSekolah]);
+
+  // useEffect(() => {
+  //   if (selectedKelas === '') {
+  //     console.log('resetting');
+  //     setSelectedMurid(
+  //       allMuridKohort.filter((murid) => murid.namaSekolah === selectedSekolah)
+  //     );
+  //   } else {
+  //     setSelectedMurid(
+  //       allMuridKohort.filter(
+  //         (murid) =>
+  //           murid.namaSekolah === selectedSekolah &&
+  //           murid.namaKelas === selectedKelas
+  //       )
+  //     );
+  //   }
+  // }, [selectedKelas]);
+
+  useEffect(() => {
+    setSelectedMurid([]);
+    setSelectedSekolah('');
+    setSelectedKelas('');
+  }, [selectedTahunKohort]);
+
   return (
     <>
-      <div className='h-full p-3 lg:p-5 overflow-y-auto'>
-        <div className='text-lg font-bold uppercase'>
-          CARIAN MURID KOHORT PROGRAM KUMURAN BERFLOURIDA
+      <div className='px-3 lg:px-7 h-full p-2 overflow-y-auto'>
+        <div className='relative shadow-md drop-shadow-sm'>
+          <h5 className='text-sm lg:text-xl font-semibold flex flex-row pl-2 lg:pl-12 pt-2'>
+            CARIAN MURID KOHORT TAHUN 1 PROGRAM KUMURAN BERFLOURIDA
+          </h5>
         </div>
-        <div className='grid grid-row-2 lg:grid-cols-2 gap-2'>
-          <div className='grid lg:grid-cols-2 gap-2'>
-            <div className='m-auto w-full lg:flex lg:flex-row lg:py-2 lg:w-96'>
-              <label
-                htmlFor='pilihanTarikh'
-                className='whitespace-nowrap flex items-center mb-1'
-              >
-                Sekolah :{' '}
+        <div className='grid grid-row-2 shadow-md shadow-user3'>
+          <div className='grid grid-cols-2 shadow-md shadow-user3 mt-5'>
+            <div className='m-5'>
+              <label className='flex justify-center items-center'>
+                <span className='font-semibold'>SEKOLAH:</span>
+                <select
+                  className='ml-2'
+                  onChange={(event) => setSelectedSekolah(event.target.value)}
+                >
+                  <option value=''>Sila pilih sekolah</option>
+                  {allSekolahKohortFMR.map((sekolah, index) => {
+                    return (
+                      <option value={sekolah} key={index}>
+                        {sekolah}
+                      </option>
+                    );
+                  })}
+                </select>
               </label>
-              <select
-                name='pilihanSekolah'
-                className='w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline'
-                onChange={(e) => setPilihanSekolah(e.target.value)}
-                value={pilihanSekolah}
-              >
-                <option value=''>Sila Pilih</option>
-                {allSekolahKohortFMR.map((sekolah, index) => (
-                  <option key={index} value={sekolah}>
-                    {sekolah}
-                  </option>
-                ))}
-              </select>
             </div>
-            <div className='m-auto w-full lg:flex lg:flex-row lg:py-2 lg:w-96'>
-              <label
-                htmlFor='pilihanTarikh'
-                className='whitespace-nowrap flex items-center mb-1'
-              >
-                Kelas :{' '}
+            <div className='m-5'>
+              <label className='flex justify-center items-center'>
+                <span className='font-semibold'>KELAS:</span>
+                <select
+                  className='ml-2'
+                  onChange={(event) => setSelectedKelas(event.target.value)}
+                >
+                  <option value=''>Sila pilih kelas</option>
+                  {allSekolahKohortFMR !== '' &&
+                    allKelasBasedOnSekolah.map((kelas, index) => {
+                      return (
+                        <option value={kelas} key={index}>
+                          {kelas}
+                        </option>
+                      );
+                    })}
+                </select>
               </label>
-              <select
-                name='pilihanKelas'
-                className='w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline'
-                onChange={(e) => setPilihanKelas(e.target.value)}
-                value={pilihanKelas}
-              >
-                <option value=''>Sila Pilih</option>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-              </select>
             </div>
           </div>
-          <div>
-            <div className='m-auto w-96 lg:flex lg:flex-row'>
-              <label
-                htmlFor='jenis-program'
-                className='whitespace-nowrap mr-7 mx-5'
-              >
-                Kohort (Tahun Mula Kumuran) :{' '}
+          <div className='grid grid-cols-2 shadow-md shadow-user3 mt-5'>
+            <div>
+              <label className='flex m-5'>
+                <span className='font-semibold'>
+                  Kohort (Tahun Mula Kumuran):
+                </span>
+                <select
+                  className='ml-2'
+                  onChange={(event) => {
+                    setSelectedTahunKohort(event.target.value);
+                  }}
+                >
+                  <option value=''>Sila pilih kohort</option>
+                  {allTahunKohort.map((kohort, index) => {
+                    return (
+                      <option value={kohort} key={index}>
+                        {kohort}
+                      </option>
+                    );
+                  })}
+                </select>
               </label>
-              <select
-                value={tahunMulaKumuran}
-                onChange={(e) => {
-                  setTahunMulaKumuran(e.target.value);
-                }}
-                className='w-full my-3 mx-4 leading-7 px-3 py-2 ring-2 focus:ring-2 focus:ring-user1 focus:outline-none rounded-md shadow-md'
-              >
-                <option value=''>Sila Pilih</option>
-                <option value='2020'>2020</option>
-                <option value='2019'>2019</option>
-                <option value='2018'>2018</option>
-              </select>
             </div>
           </div>
         </div>
-        <div className='m-auto overflow-x-auto overflow-y-hidden text-xs lg:text-sm rounded-md h-min max-w-max'>
+        <div className='m-auto overflow-x-auto overflow-y-hidden text-xs lg:text-sm rounded-md h-min max-w-max mt-5'>
           <table className='table-auto'>
             <thead className='text-userWhite bg-user2'>
               <tr>
@@ -226,72 +253,66 @@ export default function UserCarianPromosi() {
                 <th className='px-2 py-1 outline outline-1 outline-offset-1'>
                   JUMLAH KUMURAN SEMASA
                 </th>
-                <th className='px-2 py-1 outline outline-1 outline-offset-1 '>
-                  HAPUS
-                </th>
+                {userinfo.role === 'admin' ? (
+                  <th className='px-2 py-1 outline outline-1 outline-offset-1 '>
+                    HAPUS
+                  </th>
+                ) : null}
               </tr>
             </thead>
             {!isLoading &&
-              allPersonInKohort.length > 0 &&
-              allPersonInKohort.map((singlePersonKohort, index) => {
-                return (
-                  <tbody className='bg-user4'>
-                    <tr>
-                      <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
-                        {index + 1}
-                      </td>
-                      <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
-                        {singlePersonKohort.nama}
-                      </td>
-                      <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
-                        {singlePersonKohort.kumuranTerakhir}
-                      </td>
-                      <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
-                        {singlePersonKohort.jumlahKumuran}
-                      </td>
-                      {/* <td
-                        className={`${
-                          pilihanId === singlePersonKohort._id && 'bg-user3'
-                        } px-2 py-3 outline outline-1 outline-userWhite outline-offset-1 text-user2`}
-                      >
-                        {hasilQueryPromosi.statusReten === 'telah diisi' ||
-                        hasilQueryPromosi.statusReten === 'reten salah' ? (
-                          <Link
-                            target='_blank'
-                            rel='noreferrer'
-                            to={`${
-                              singlePersonKohort.promosiIndividu
-                                ? `/pengguna/landing/promosi-individu/form-promosi/${singlePersonKohort._id}`
-                                : `/pengguna/landing/promosi-klinik/form-promosi/${singlePersonKohort._id}`
-                            }`}
-                            className='m-2 p-2 uppercase bg-user3 text-sm text-userWhite rounded-md shadow-md whitespace-nowrap hover:bg-user1 transition-all'
-                          >
-                            lihat reten
-                          </Link>
+              allMuridKohort.length > 0 &&
+              allMuridKohort
+                .filter((muridKohort) => {
+                  return muridKohort.tahunKohortFMR === selectedTahunKohort;
+                })
+                .filter((murid) => {
+                  if (selectedSekolah === '') {
+                    return murid;
+                  } else {
+                    if (selectedKelas === '') {
+                      return murid.namaSekolah === selectedSekolah;
+                    } else {
+                      return (
+                        murid.namaSekolah === selectedSekolah &&
+                        murid.namaKelas === selectedKelas
+                      );
+                    }
+                  }
+                })
+                .map((singlePersonKohort, index) => {
+                  return (
+                    <tbody className='bg-user4'>
+                      <tr>
+                        <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
+                          {index + 1}
+                        </td>
+                        <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
+                          {singlePersonKohort.nama}
+                        </td>
+                        <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
+                          {singlePersonKohort.tarikhKumuranKohortFMR &&
+                          singlePersonKohort.tarikhKumuranKohortFMR.length > 0
+                            ? moment(
+                                singlePersonKohort.tarikhKumuranKohortFMR[
+                                  singlePersonKohort.tarikhKumuranKohortFMR
+                                    .length - 1
+                                ]
+                              ).format('DD/MM/YYYY')
+                            : 'Belum mula'}
+                        </td>
+                        <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1'>
+                          {singlePersonKohort.jumlahKumuran}
+                        </td>
+                        {userinfo.role === 'admin' ? (
+                          <td className='px-2 py-1 outline outline-1 outline-userWhite outline-offset-1 text-center'>
+                            <BsTrash className='text-xl' />
+                          </td>
                         ) : null}
-                        {(userinfo.role === 'admin' ||
-                          userinfo.rolePromosiKlinik) &&
-                          (singlePersonKohort.statusReten === 'telah diisi' ||
-                          singlePersonKohort.statusReten === 'reten salah' ? (
-                            <span
-                              onClick={() => {
-                                setModalHapusPromosi(true);
-                                setPilihanId(singlePersonKohort._id);
-                                setPilihanNama(singlePersonKohort.namaAcara);
-                                // setPilihanStatusReten(
-                                //   hasilQueryPromosi.statusReten
-                                // );
-                              }}
-                              className='m-2 p-2 uppercase bg-user9 text-sm text-userWhite rounded-md shadow-md whitespace-nowrap hover:bg-user1 hover:cursor-pointer transition-all'
-                            >
-                              HAPUS RETEN
-                            </span>
-                          ) : null)}
-                      </td> */}
-                    </tr>
-                  </tbody>
-                );
-              })}
+                      </tr>
+                    </tbody>
+                  );
+                })}
             {isLoading && (
               <tbody className='bg-user4'>
                 <tr>
