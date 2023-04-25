bb76b545027f5e9dcfe2b1cd469e08efe11ac8cd
c4lyp5o <calypso.hellhammer@gmail.com>
2023-04-25 23:27:35 +0800

    feat: buat controller ondemand

---
+++ controllers/ondemand.js
@@ -0,0 +1,45 @@
+const Ondemand = require('../models/Ondemand');
+const { logger } = require('../logs/logger');
+
+// GET / - untuk dapatkan ondemand setting
+const getCurrentOndemandSetting = async (req, res) => {
+  logger.info(
+    `${req.method} ${req.url} [OndemandController] getCurrentOndemandSetting called`
+  );
+
+  const currentOndemandSetting = await Ondemand.findOne({
+    name: 'current',
+  });
+
+  if (!currentOndemandSetting) {
+    return res.status(404).json({ msg: 'No current ondemand setting found' });
+  }
+
+  res.status(200).json({ currentOndemandSetting });
+};
+
+// PATCH / - untuk update ondemand setting
+const updateCurrentOndemandSetting = async (req, res) => {
+  logger.info(
+    `${req.method} ${req.url} [OndemandController] updateCurrentOndemandSetting called`
+  );
+
+  const { ondemandSetting } = req.body;
+
+  const currentOndemandSetting = await Ondemand.findOneAndUpdate(
+    { name: 'current' },
+    { $set: { ondemandSetting } },
+    { new: true }
+  );
+
+  if (!currentOndemandSetting) {
+    return res.status(404).json({ msg: 'No current ondemand setting found' });
+  }
+
+  res.status(200).json({ currentOndemandSetting });
+};
+
+module.exports = {
+  getCurrentOndemandSetting,
+  updateCurrentOndemandSetting,
+};
