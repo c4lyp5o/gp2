55facd8b0790101564ede6a8f364d44089631d57
c4lyp5o <calypso.hellhammer@gmail.com>
2023-04-26 16:19:22 +0800

    fix: buat entry untuk ondemand kalau x pernah wujud di db

--- controllers/ondemand.js
+++ controllers/ondemand.js
@@ -12,7 +12,28 @@ const getCurrentOndemandSetting = async (req, res) => {
   });
 
   if (!currentOndemandSetting) {
-    return res.status(404).json({ msg: 'No current ondemand setting found' });
+    const currentOndemandSetting = await Ondemand.create({
+      name: 'current',
+      adminPage: true,
+      PG101A: true,
+      PG101C: true,
+      PG211A: true,
+      PG211C: true,
+      PG206: true,
+      PG207: true,
+      PG214: true,
+      PGPR201: true,
+      PGPRO01: true,
+      PGPRO01Combined: true,
+      PG201P2: true,
+      PGS203P2: true,
+      TODP1: true,
+      MASA: true,
+      BP: true,
+      BPE: true,
+      GENDER: true,
+    });
+    return res.status(200).json({ currentOndemandSetting });
   }
 
   res.status(200).json({ currentOndemandSetting });
