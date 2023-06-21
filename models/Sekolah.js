1b680f71ab8cc058b7ab31cc202be60525aa14fb
noct <ezy_95@outlook.com>
2023-06-21 15:11:11 +0800

    chore: comment on statusRawatan sekolah

--- models/Sekolah.js
+++ models/Sekolah.js
@@ -9,11 +9,11 @@ const SekolahSchema = new mongoose.Schema(
       enum: {
         values: [
           'belum mula',
-          'enggan',
-          'tidak hadir',
+          'enggan', // xda pemeriksaan xda rawatan isi ada
+          'tidak hadir', // xda pemeriksaan xda rawatan isi xda
           'belum selesai',
-          'enggan rawatan',
-          'tidak hadir rawatan',
+          'enggan rawatan', // ada pemeriksaan xda rawatan
+          'tidak hadir rawatan', // ada pemeriksaan xda rawatan isi rawatan xda
           'selesai',
         ],
         message:
