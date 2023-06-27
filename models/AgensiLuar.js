f6de851327a1b14125bf723e5dafa43857082085
Dvachini <izzuddinazman5@gmail.com>
2023-06-27 08:11:37 +0800

    feat: model pp and jp

--- models/AgensiLuar.js
+++ models/AgensiLuar.js
@@ -10,6 +10,8 @@ const agensiLuarSchema = new Schema(
     // Program G-tod --------------------------------------------------
     jenisAgensiLuar: { type: String, default: 'NOT APPLICABLE' },
     namaAgensiLuar: { type: String, default: 'NOT APPLICABLE' },
+    bilPegawaiPergigian: { type: Number, default: 0 },
+    bilJuruterapi: { type: Number, default: 0 },
     namaTaskaTadika: { type: String, default: 'NOT APPLICABLE' },
     alamatTaskaTadika: { type: String, default: 'NOT APPLICABLE' },
     // pemeriksaan -------------------------------------------------------
