437a753e1d9dd35f287241631a018f7e493e24e1
Dvachini <izzuddinazman5@gmail.com>
2023-07-05 15:22:02 +0800

    feat: tambah input jumlah enggan dan tidak hadir

--- client/src/admin/components/Modal.jsx
+++ client/src/admin/components/Modal.jsx
@@ -715,6 +715,20 @@ const EditModalForKp = ({
           ) {
             res.data.enrolmenMuridOaPenan = 0;
           }
+          if (
+            res.data.jumlahEngganTasTad === 'NOT APPLICABLE' ||
+            res.data.jumlahEngganTasTad === null ||
+            res.data.jumlahEngganTasTad === undefined
+          ) {
+            res.data.jumlahEngganTasTad = 0;
+          }
+          if (
+            res.data.jumlahTidakHadirTasTad === 'NOT APPLICABLE' ||
+            res.data.jumlahTidakHadirTasTad === null ||
+            res.data.jumlahTidakHadirTasTad === undefined
+          ) {
+            res.data.jumlahTidakHadirTasTad = 0;
+          }
         }
         // console.log(res.data);
         setEditedEntity(res.data);
@@ -811,6 +825,8 @@ const EditModalForKp = ({
           editedEntity.enrolmenMuridBerkeperluanKhas,
         enrolmenMuridOaPenan: editedEntity.enrolmenMuridOaPenan,
         jenisTadikaKerajaan: editedEntity.jenisTadikaKerajaan,
+        jumlahEngganTasTad: editedEntity.jumlahEngganTasTad,
+        jumlahTidakHadirTasTad: editedEntity.jumlahTidakHadirTasTad,
       };
     }
     if (FType === 'kpb' || FType === 'mpb') {
