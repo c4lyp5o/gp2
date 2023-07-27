716a09c1d9af146d9cd1b2f3c75ccdefb569620d
Dvachini <izzuddinazman5@gmail.com>
2023-07-27 12:17:30 +0800

    feat: routes kotak delete

--- routes/kohortKotak.js
+++ routes/kohortKotak.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const {
   getSinglePersonKohortKotak,
   updatePersonKohortKotak,
+  softDeletePersonKOTAK,
   deletePersonKohortKotak,
   queryPersonKohortKotak,
 } = require('../controllers/kohortKotak');
 
 router.route('/').get(queryPersonKohortKotak);
+
+// PATCH
 router
   .route('/:personKohortKotakId')
   .get(getSinglePersonKohortKotak)
   .patch(updatePersonKohortKotak);
+router.route('/delete/:personKohortKotakId').patch(softDeletePersonKOTAK);
 
 module.exports = router;
