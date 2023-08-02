ea1e5725733e24658eca90a02241c10792e596d3
Dvachini <izzuddinazman5@gmail.com>
2023-08-02 11:15:06 +0800

    style: background confirmation t1

--- client/src/user/components/sekolah/UserTambahT1Sekolah.jsx
+++ client/src/user/components/sekolah/UserTambahT1Sekolah.jsx
@@ -430,7 +430,7 @@ function UserTambahT1Sekolah({
       </div>
       <div
         onClick={closeModal}
-        className='fixed inset-0 bg-userBlack opacity-50 z-0'
+        className='absolute inset-0 bg-user1 opacity-75 z-10'
       />
     </>
   );
