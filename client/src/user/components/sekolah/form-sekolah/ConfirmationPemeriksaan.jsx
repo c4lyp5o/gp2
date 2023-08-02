04a97b68dc6e3143cd771b61abcff6d20be3ff47
Dvachini <izzuddinazman5@gmail.com>
2023-08-02 11:14:22 +0800

    style: background confirmation pemeriksaan

--- client/src/user/components/sekolah/form-sekolah/ConfirmationPemeriksaan.jsx
+++ client/src/user/components/sekolah/form-sekolah/ConfirmationPemeriksaan.jsx
@@ -714,7 +714,7 @@ const ConfirmModal = ({ children, data }) => {
           </div>
           <div
             onClick={closeModal}
-            className='fixed inset-0 bg-userBlack opacity-50 z-0'
+            className='absolute inset-0 bg-user1 opacity-75 z-10'
           />
         </>
       )}
