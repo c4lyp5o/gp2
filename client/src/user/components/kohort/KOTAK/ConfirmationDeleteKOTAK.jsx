7bbeb16534ca87d4f54f6e48f6c7fd98813471bd
Dvachini <izzuddinazman5@gmail.com>
2023-08-03 10:02:08 +0800

    style: betulkan confirmation before delete sekolah

--- client/src/user/components/kohort/KOTAK/ConfirmationDeleteKOTAK.jsx
+++ client/src/user/components/kohort/KOTAK/ConfirmationDeleteKOTAK.jsx
@@ -25,7 +25,6 @@ function ConfirmationDeleteKotak({
     setMenerimaNasihatRingkas('');
   };
 
-  // TODO kena cantikkan lagi hehe
   return (
     <>
       <form
@@ -40,81 +39,95 @@ function ConfirmationDeleteKotak({
           <h5 className='bg-user9 text-userWhite font-semibold text-xl h-7'>
             PERHATIAN
           </h5>
-          <div className='mt-1 py-1'>
+          <div className='mt-1 py-1 px-4'>
             <span className='relative flex items-center justify-center mt-4'>
               <FaUserCheck className='text-4xl text-user9 mx-auto absolute animate-ping' />
               <FaUserCheck className='text-4xl text-user9 mx-auto absolute' />
             </span>
-            <p className='px-1 font-semibold mt-7'>
-              Sila isi maklumat berikut sekiranya ingin menghapuskan pelajar
+            <p className='px-1 font-semibold my-7 flex flex-col normal-case'>
+              <span>
+                Sila isi maklumat berikut sekiranya ingin menghapuskan pelajar
+              </span>
               {nama}
             </p>
-            <div className='col-span-2 grid grid-cols-[3fr_2fr]'>
-              <p className='flex items-center text-sm normal-case'>
-                Melaksanakan saringan merokok melalui Program KOTAK
-                <span className='text-user6'>*</span>
+            <div className='grid grid-cols-[3fr_2fr] my-1'>
+              <p className='flex flex-row text-left text-sm normal-case whitespace-pre-wrap '>
+                <span>
+                  Melaksanakan saringan merokok melalui Program KOTAK
+                  <b className='text-user6'>*</b>
+                </span>
               </p>
-              <div className='flex items-center'>
-                <input
-                  required
-                  type='radio'
-                  name='melaksanakan-saringan-merokok'
-                  id='ya-melaksanakan-saringan-merokok'
-                  value='ya-melaksanakan-saringan-merokok'
-                  checked={
-                    melaksanakanSaringanMerokok ===
-                    'ya-melaksanakan-saringan-merokok'
-                      ? true
-                      : false
-                  }
-                  onChange={(e) => {
-                    setMelaksanakanSaringanMerokok(e.target.value);
-                    setStatusM('');
-                    setMenerimaNasihatRingkas('');
-                  }}
-                  className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500'
-                />
+              <div className='grid grid-cols-2 gap-2 my-2'>
                 <label
                   htmlFor='ya-melaksanakan-saringan-merokok'
-                  className='mx-2 text-sm font-m'
-                >
-                  Ya
-                </label>
-                <input
-                  required
-                  type='radio'
-                  name='melaksanakan-saringan-merokok'
-                  id='tidak-melaksanakan-saringan-merokok'
-                  value='tidak-melaksanakan-saringan-merokok'
-                  checked={
+                  className={`flex justify-center items-center py-1 px-1 rounded-md shadow-sm shadow-user1 ${
                     melaksanakanSaringanMerokok ===
-                    'tidak-melaksanakan-saringan-merokok'
-                      ? true
-                      : false
-                  }
-                  onChange={(e) => {
-                    setMelaksanakanSaringanMerokok(e.target.value);
-                    setStatusM('');
-                    setMenerimaNasihatRingkas('');
-                  }}
-                  className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500'
-                />
+                    'ya-melaksanakan-saringan-merokok'
+                      ? ' ring ring-offset-user12 bg-user4 bg-opacity-30 transition-all duration-500'
+                      : ''
+                  }`}
+                >
+                  <input
+                    required
+                    type='radio'
+                    name='melaksanakan-saringan-merokok'
+                    id='ya-melaksanakan-saringan-merokok'
+                    value='ya-melaksanakan-saringan-merokok'
+                    checked={
+                      melaksanakanSaringanMerokok ===
+                      'ya-melaksanakan-saringan-merokok'
+                        ? true
+                        : false
+                    }
+                    onChange={(e) => {
+                      setMelaksanakanSaringanMerokok(e.target.value);
+                      setStatusM('');
+                      setMenerimaNasihatRingkas('');
+                    }}
+                    className='hidden'
+                  />
+                  <span>Ya</span>
+                </label>
                 <label
                   htmlFor='tidak-melaksanakan-saringan-merokok'
-                  className='mx-2 text-sm font-m'
+                  className={`flex justify-center items-center py-1 px-1 rounded-md shadow-sm shadow-user1 ${
+                    melaksanakanSaringanMerokok ===
+                    'tidak-melaksanakan-saringan-merokok'
+                      ? ' ring ring-offset-user12 bg-user4 bg-opacity-30 transition-all duration-500'
+                      : ''
+                  }`}
                 >
-                  Tidak
+                  <input
+                    required
+                    type='radio'
+                    name='melaksanakan-saringan-merokok'
+                    id='tidak-melaksanakan-saringan-merokok'
+                    value='tidak-melaksanakan-saringan-merokok'
+                    checked={
+                      melaksanakanSaringanMerokok ===
+                      'tidak-melaksanakan-saringan-merokok'
+                        ? true
+                        : false
+                    }
+                    onChange={(e) => {
+                      setMelaksanakanSaringanMerokok(e.target.value);
+                      setStatusM('');
+                      setMenerimaNasihatRingkas('');
+                    }}
+                    className='hidden'
+                  />
+                  <span>tidak</span>
                 </label>
               </div>
             </div>
             {melaksanakanSaringanMerokok ===
               'ya-melaksanakan-saringan-merokok' && (
-              <div className='text-sm grid grid-cols-[3fr_2fr] '>
+              <div className='text-sm grid grid-cols-[3fr_2fr] my-1 '>
                 <label
                   htmlFor='statusM'
                   className='flex items-center text-sm normal-case'
                 >
-                  Status Merokok<span className='text-user6'>*</span>
+                  Status Merokok<b className='text-user6'>*</b>
                 </label>
                 <select
                   required
@@ -124,7 +137,7 @@ function ConfirmationDeleteKotak({
                   onChange={(e) => {
                     setStatusM(e.target.value);
                   }}
-                  className='appearance-none w-36 border-b-4 border-b-user4 py-1 px-2 text-base focus:border-b-user2 focus:outline-none drop-shadow-lg'
+                  className='appearance-none text-sm uppercase w-full px-2 py-1 text-user1 border border-user1 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-user1 focus:border-transparent'
                 >
                   <option value=''></option>
                   {/* <option value='perokok-semasa'>Perokok Semasa</option> */}
@@ -139,56 +152,65 @@ function ConfirmationDeleteKotak({
             )}
             {melaksanakanSaringanMerokok ===
               'ya-melaksanakan-saringan-merokok' && (
-              <div className='col-span-2 grid grid-cols-[3fr_2fr]'>
+              <div className='grid grid-cols-[3fr_2fr] my-1'>
                 <p className='flex items-center text-sm normal-case'>
                   Pesakit menerima nasihat ringkas?
-                  <span className='text-user6'>*</span>
+                  <b className='text-user6'>*</b>
                 </p>
-                <div className='flex items-center'>
-                  <input
-                    required
-                    type='radio'
-                    name='menerima-nasihat-ringkas'
-                    id='ya-menerima-nasihat-ringkas'
-                    value='ya-menerima-nasihat-ringkas'
-                    checked={
-                      menerimaNasihatRingkas === 'ya-menerima-nasihat-ringkas'
-                        ? true
-                        : false
-                    }
-                    onChange={(e) => {
-                      setMenerimaNasihatRingkas(e.target.value);
-                    }}
-                    className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500'
-                  />
+                <div className='grid grid-cols-2 gap-2 my-2'>
                   <label
                     htmlFor='ya-menerima-nasihat-ringkas'
-                    className='mx-2 text-sm font-m'
+                    className={`flex justify-center items-center py-1 px-1 rounded-md shadow-sm shadow-user1 ${
+                      menerimaNasihatRingkas === 'ya-menerima-nasihat-ringkas'
+                        ? ' ring ring-offset-user12 bg-user4 bg-opacity-30 transition-all duration-500'
+                        : ''
+                    }`}
                   >
-                    Ya
+                    <input
+                      required
+                      type='radio'
+                      name='menerima-nasihat-ringkas'
+                      id='ya-menerima-nasihat-ringkas'
+                      value='ya-menerima-nasihat-ringkas'
+                      checked={
+                        menerimaNasihatRingkas === 'ya-menerima-nasihat-ringkas'
+                          ? true
+                          : false
+                      }
+                      onChange={(e) => {
+                        setMenerimaNasihatRingkas(e.target.value);
+                      }}
+                      className='hidden'
+                    />
+                    <span>Ya</span>
                   </label>
-                  <input
-                    required
-                    type='radio'
-                    name='menerima-nasihat-ringkas'
-                    id='tidak-menerima-nasihat-ringkas'
-                    value='tidak-menerima-nasihat-ringkas'
-                    checked={
-                      menerimaNasihatRingkas ===
-                      'tidak-menerima-nasihat-ringkas'
-                        ? true
-                        : false
-                    }
-                    onChange={(e) => {
-                      setMenerimaNasihatRingkas(e.target.value);
-                    }}
-                    className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500'
-                  />
                   <label
                     htmlFor='tidak-menerima-nasihat-ringkas'
-                    className='mx-2 text-sm font-m'
+                    className={`flex justify-center items-center py-1 px-1 rounded-md shadow-sm shadow-user1 ${
+                      menerimaNasihatRingkas ===
+                      'tidak-menerima-nasihat-ringkas'
+                        ? ' ring ring-offset-user12 bg-user4 bg-opacity-30 transition-all duration-500'
+                        : ''
+                    }`}
                   >
-                    Tidak
+                    <input
+                      required
+                      type='radio'
+                      name='menerima-nasihat-ringkas'
+                      id='tidak-menerima-nasihat-ringkas'
+                      value='tidak-menerima-nasihat-ringkas'
+                      checked={
+                        menerimaNasihatRingkas ===
+                        'tidak-menerima-nasihat-ringkas'
+                          ? true
+                          : false
+                      }
+                      onChange={(e) => {
+                        setMenerimaNasihatRingkas(e.target.value);
+                      }}
+                      className='hidden'
+                    />
+                    <span>tidak</span>
                   </label>
                 </div>
               </div>
