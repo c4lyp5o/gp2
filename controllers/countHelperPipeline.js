abf045fa28cb94d283124bcf58cff953cb777207
c4lyp5o <calypso.hellhammer@gmail.com>
2023-08-04 12:15:22 +0800

    fix: mbk, kalau ada gigi susu, kena dfx 0

--- controllers/countHelperPipeline.js
+++ controllers/countHelperPipeline.js
@@ -6110,6 +6110,23 @@ const groupPemeriksaanBiasa = {
             },
             {
               $and: [
+                { $lt: ['$umur', 1] },
+                {
+                  $eq: [
+                    '$yaTidakPesakitMempunyaiGigi',
+                    'ya-pesakit-mempunyai-gigi',
+                  ],
+                },
+                {
+                  $eq: ['$adaDesidusPemeriksaanUmum', true],
+                },
+                { $eq: ['$dAdaGigiDesidusPemeriksaanUmum', 0] },
+                { $eq: ['$fAdaGigiDesidusPemeriksaanUmum', 0] },
+                { $eq: ['$xAdaGigiDesidusPemeriksaanUmum', 0] },
+              ],
+            },
+            {
+              $and: [
                 { $gte: ['$umur', 1] },
                 { $lte: ['$umur', 17] },
                 { $eq: ['$adaDesidusPemeriksaanUmum', true] },
@@ -6149,6 +6166,12 @@ const groupPemeriksaanBiasa = {
             {
               $and: [
                 { $gte: ['$umur', 5] },
+                {
+                  $eq: [
+                    '$yaTidakPesakitMempunyaiGigi',
+                    'ya-pesakit-mempunyai-gigi',
+                  ],
+                },
                 { $eq: ['$adaKekalPemeriksaanUmum', true] },
                 { $eq: ['$dAdaGigiKekalPemeriksaanUmum', 0] },
                 { $eq: ['$mAdaGigiKekalPemeriksaanUmum', 0] },
