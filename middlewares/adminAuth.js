741796fc9e1b065574edd558a2676afb422a8efa
c4lyp5o <calypso.hellhammer@gmail.com>
2023-08-10 11:21:48 +0800

    fix: tambah user id dalam req.user

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -47,6 +47,7 @@ const adminAuthInt = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = {
+      userId: decoded.userId,
       userAccount: decoded.userAccount,
       accountType: decoded.accountType,
     };
